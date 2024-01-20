31357c7391094dce7701f061ae725d8ca2c0c0cf
Ethan Guerin <eguerin@ewu.edu>
2024-01-20 15:09:15 -0800

    Assignment2 - add: Finished and added FileLogger and BaseLogger

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -9,8 +9,9 @@ public class LogFactoryTests
     [TestMethod]
     public void LogFactory_CreateLoggerWithClassName_ClassNameSetCorrectly()
     {
-        var logFactory = new LogFactory();
+        var filePath = "test.log";
         var expectedClassName = "TestClass";
+        var logger = new FileLogger(filePath) { ClassName = expectedClassName };
 
         Assert.IsNotNull(logger);
         Assert.AreEqual(expectedClassName, logger.ClassName);
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,10 +1,21 @@
-﻿namespace Logger;
+namespace Logger;
 
 public class LogFactory
 {
+    private string fileLoggerPath;
+
+    public void ConfigureFileLogger(string filePath)
+    {
+        this.fileLoggerPath = filePath;
+    }
+
     public BaseLogger CreateLogger(string className)
     {
-
+        if(fileLoggerPath != null) {
+            var fileLogger = new FileLogger(fileLoggerPath);
+            fileLogger.ClassName = className;
+            return fileLogger;
+        }
         return null;
     }
 }
