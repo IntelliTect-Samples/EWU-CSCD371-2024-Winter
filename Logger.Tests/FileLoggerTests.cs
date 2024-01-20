e6e95c9f8512aaafc21340738e498ffa6f56580f
Ethan Guerin <eguerin@ewu.edu>
2024-01-20 14:25:12 -0800

    Assignment2 - test: Added Test for FileLogger

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class FileLoggerTests
 {
+    [TestMethod]
+    public void FileLogger_Log_MessageAppendedToFile()
+    {
+        var filePath = 'test.log';
+        var logger = new FileLoggerTests(filePath) { ClassName = 'TestClass' };
+        var logMessage = "Test Message";
 
+        logger.Log(LogLevel.Information, logMessage);
+
+        string[] lines = File.ReadAllLines(filePath);
+        Assert.AreEqual($"{DateTime.Now} TestClass Information: {logMessage}", lines[^1]);
+    }
+    
 }
