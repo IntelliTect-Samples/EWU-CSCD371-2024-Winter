b4433e3d9c5437a6aa8cc371c301b3ebac7e0826
Ethan Guerin <eguerin@ewu.edu>
2024-01-20 14:45:39 -0800

    Assignment2 - feat: Created FileLogger Class

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -9,13 +9,13 @@ public class FileLoggerTests
     [TestMethod]
     public void FileLogger_Log_MessageAppendedToFile()
     {
-        var filePath = 'test.log';
-        var logger = new FileLoggerTests(filePath) { ClassName = 'TestClass' };
+        var filePath = "test.log";
+        var logger = new FileLogger(filePath) { ClassName = "TestClass" };
         var logMessage = "Test Message";
 
         logger.Log(LogLevel.Information, logMessage);
 
-        string[] lines = File.ReadAllLines(filePath);
+        string[] lines = System.IO.File.ReadAllLines(filePath);
         Assert.AreEqual($"{DateTime.Now} TestClass Information: {logMessage}", lines[^1]);
     }
     
---
+++ Logger/FileLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Logger
+{
+    public class FileLogger : BaseLogger
+    {
+        private readonly string filePath;
+        public FileLogger(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public override void Log(LogLevel logLevel, string message)
+        {
+            string logEntry = $"{DateTime.Now} {ClassName} {logLevel}: {message}";
+
+            File.AppendAllText(filePath, logEntry + Environment.NewLine);
+        }
+    }
+}
