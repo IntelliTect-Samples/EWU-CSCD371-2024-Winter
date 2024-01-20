8ea91b2845b5433aea27b4d8d488f3dff35fcebd
Ethan Guerin <eguerin@ewu.edu>
2024-01-20 14:13:02 -0800

    Assignment2 - test: Added Test for BaseLogger

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class LogFactoryTests
 {
+        var logger = logFactory.CreateLogger(expectedClassName);
+    [TestMethod]
+    public void LogFactory_CreateLoggerWithClassName_ClassNameSetCorrectly()
+    {
+        var logFactory = new LogFactory();
+        var expectedClassName = "TestClass";
 
+        Assert.IsNotNull(logger);
+        Assert.AreEqual(expectedClassName, logger.ClassName);
+
+    }
+    
 }
