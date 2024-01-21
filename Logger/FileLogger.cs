221007fe755dfd85ba83617eb29650981d3d3c14
Andree Ramirez <aramirez61@ewu.edu>
2024-01-20 16:05:47 -0800

    feat: add className auto properties and set them
    added className to BaseLogger with get and set method, and set it in the LogFactory using object initializer.

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,9 @@
+namespace Logger;
+
+public class FileLogger : BaseLogger
+{
+    public override void Log(LogLevel logLevel, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+}
