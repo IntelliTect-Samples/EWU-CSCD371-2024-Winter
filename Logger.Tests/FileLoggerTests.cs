b7cd21e09a2f461507af2cecde1f129eb5a49efa
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-01-21 13:18:10 -0800

    added Log_ValidMessage_WritesToFileCorrectly failing test

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class FileLoggerTests
 {
+
+    [TestMethod]
+    [DataRow("10/7/2019 12:38:59 AM FileLoggerTests Warning: Test message", LogLevel.Warning, "@FakePath", "Logger", "Test message" )]
+    public void Log_ValidMessage_WritesToFileCorrectly(string expectedContent, LogLevel level, string pathname, string className, string message)
+    {
+        // Arrange
+        LogFactory logFactory = new LogFactory();
+        logFactory.ConfigureFileLogger(pathname);
+        FileLogger? fileLogger = logFactory.CreateLogger(className) as FileLogger;
+
+        // Act
+        fileLogger?.Log(level, message);
+        string fileContents = File.ReadAllText(pathname);
+
+        // Assert
+        Assert.AreEqual(expectedContent, fileContents);
+
+    }
 
 }
