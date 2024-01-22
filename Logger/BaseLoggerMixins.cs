8737578b780627a51b52ead4328c0b97a1d23eef
Andree Ramirez <aramirez61@ewu.edu>
2024-01-22 10:19:22 -0800

    Added extension methods for Warning, Information, Debug

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -13,4 +13,40 @@ public static class BaseLoggerMixins
             baseLogger.Log(LogLevel.Error, string.Format(message, arguments);
         }
     }
+
+    public static void Warning(this BaseLogger baseLogger, string message, params object[] arguments)
+    {
+        if (baseLogger == null)
+        {
+            throw new System .ArgumentNullException(nameof(baseLogger));
+        }
+        else
+        {
+            baseLogger.Log(LogLevel.Warning, string.Format(message, arguments);
+        }
+    }
+
+    public static void Information(this BaseLogger baseLogger, string message, params object[] arguments)
+    {
+        if (baseLogger == null)
+        {
+            throw new System .ArgumentNullException(nameof(baseLogger));
+        }
+        else
+        {
+            baseLogger.Log(LogLevel.Information, string.Format(message, arguments);
+        }
+    }
+
+    public static void Debug(this BaseLogger baseLogger, string message, params object[] arguments)
+    {
+        if (baseLogger == null)
+        {
+            throw new System .ArgumentNullException(nameof(baseLogger));
+        }
+        else
+        {
+            baseLogger.Log(LogLevel.Debug, string.Format(message, arguments);
+        }
+    }
 }
