928db4ab84aa4dc31bbebd51f04db1bfcb44ef82
Hadi <amallah@ewu.edu>
2024-01-22 10:16:38 -0800

    Extension method for Error Done

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -2,5 +2,15 @@
 
 public static class BaseLoggerMixins
 {
-
+    public static void Error(this BaseLogger baseLogger, string message, params object[] arguments)
+    {
+        if (baseLogger == null)
+        {
+            throw new System .ArgumentNullException(nameof(baseLogger));
+        }
+        else
+        {
+            baseLogger.Log(LogLevel.Error, string.Format(message, arguments);
+        }
+    }
 }
