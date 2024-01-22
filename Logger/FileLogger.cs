b3d0c19cbba12222f673905f8c62d5b0ed750a0b
Andree Ramirez <aramirez61@ewu.edu>
2024-01-22 11:11:05 -0800

    Formatted log message in File Logger

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -11,9 +11,9 @@ public class FileLogger : BaseLogger
     public override void Log(LogLevel logLevel, string message)
     {
         string dateTime = System.DateTime.Now.ToString("M/d/yyyy hh:mm:ss tt");
-        string logEntry = $"{dateTime} [{logLevel}]: {message}";
+        string logEntry = $"{dateTime} {nameof(ClassName)} {logLevel}: {message}";
 
         //File.AppendAllText(filePath, logEntry + Environment.NewLine);
-        
+        //need to append and write to file
     }
 }
