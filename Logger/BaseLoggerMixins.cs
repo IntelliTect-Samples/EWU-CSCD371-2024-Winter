09382cd5c2d76773ac37b1a12e1e69479d6e89eb
EthanF <eflorko@ewu.edu>
2024-01-22 14:30:27 -0800

    Create error extension method
    did stuff to extension method

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,14 @@
-﻿namespace Logger;
+﻿using System.IO;
+
+namespace Logger;
 
 public static class BaseLoggerMixins
 {
+    public static void Error (this BaseLogger logger, string message, params object[] args)
+    {
+        LogWithLevel(logger, LogLevel.Error, message, args)
+    }
+
+   
 
 }
