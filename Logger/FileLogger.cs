0bc45ddf11295925bf741c8803302965c0afb606
Hadi <amallah@ewu.edu>
2024-01-22 10:34:35 -0800

    Constructor for filePath

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -2,6 +2,12 @@ namespace Logger;
 
 public class FileLogger : BaseLogger
 {
+
+    string filePath;
+    public FileLogger(string filePath)
+    {
+        this,.filePath = filePath;
+    }
     public override void Log(LogLevel logLevel, string message)
     {
         throw new System.NotImplementedException();
