143207b309d617ee06139f4a20bea491766a8b0e
EthanF <eflorko@ewu.edu>
2024-01-22 12:10:17 -0800

    Modified LogFactory
    did stuff in LogFactory to comply with assignment details

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,6 +2,7 @@
 
 public abstract class BaseLogger
 {
+    //Auto property implementation 
     public string ClassName { get; set; }
     public abstract void Log(LogLevel logLevel, string message);
 }
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,9 +2,23 @@
 
 public class LogFactory
 {
+    //Private member for file path storage ie FileLogger
+    private string fileLoggerFilePath;
+
+    //Method to set file path for FileLogger
+    public void ConfigureFileLogger(string filePath)
+    {  
+        fileLoggerFilePath = filePath; 
+    }
     public BaseLogger CreateLogger(string className)
-    {
+    {   //Check if confifgured 
+        if (!string.IsNullOrEmpty(fileLoggerFilePath))
+        {
+            //Create FileLoger and set class name
+            return new FileLogger(fileLoggerFilePath) { ClassName = className };
+        }
 
         return null;
+        
     }
 }
