9722f30d43b3d1a566bf749f6da41d5bb3bb485f
Hadi <amallah@ewu.edu>
2024-01-22 12:10:57 -0800

    Added Test to FileLoggerTests

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -5,5 +5,23 @@ namespace Logger.Tests;
 [TestClass]
 public class FileLoggerTests
 {
+    [TestMethod]
+    public void log_Path_AppendToLine()
+    {
+        string fileName = @"C:\file.txt";
+        FileLogger fileLogger = new FileLogger(fileName);
+        fileLogger.Log(LogLevel.Warning, "Warnings");
+        string log;
+
+        using(StreamReader sr = File.OpenText(fileName))
+        {
+            while (sr.EndOfStream)
+            {
+                log = sr.ReadLine();
+            }
+        }
+        Assert.AreEqual($"{System.DateTime.Now} {"FileLoggerTests"} {LogLevel.Warning}: {"Warnings"}",  log);
+    }
+    
 
 }
