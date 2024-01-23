b898c7957e092648c2f51cfa2a108c54c9f1e3ea
Nathan <nmarsee@ewu.edu>
2024-01-23 02:32:57 -0800

    Implements all required methods, creates FileLogger.cs, sets up csproj

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class LogFactoryTests
 {
+    public LogFactory LogFactory { get; set; }
+    public LogFactoryTests()
+    {
+        LogFactory = new LogFactory();
+    }
 
+    [TestMethod]
+    public void CreateBaseLogger_NoFileGiven_IsNull()
+    {
+        BaseLogger? logger = LogFactory.CreateLogger(nameof(LogFactoryTests));
+        Assert.IsNull(logger);
+    }
+
+    [TestMethod]
+    public void CreateBaseLogger_FileGiven_LoggerCreated()
+    {
+        LogFactory.ConfigureFileLogger("test");
+        BaseLogger? logger = LogFactory.CreateLogger(nameof(LogFactoryTests));
+        Assert.IsInstanceOfType(logger, typeof(FileLogger));
+    }
 }
