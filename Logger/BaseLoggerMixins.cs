14f9b898a2b18031893dbea14db779bd4ef64fdb
Ethan Guerin <eguerin@ewu.edu>
2024-01-22 21:05:15 -0800

    Assignment2 - fix: I tried

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 
 namespace Logger;
@@ -6,37 +7,25 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger? logger, string message, params object[] args)
     {
-        if(logger == null)
-        {
-            throw new System.ArgumentNullException(nameof(logger));
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Error, string.Format(CultureInfo.InvariantCulture, message, args));   
     }
 
     public static void Warning(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger == null)
-        {
-            throw new System.ArgumentNullException(nameof(logger));
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Warning, string.Format(CultureInfo.InvariantCulture, message, args));
     }
 
     public static void Information(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger == null)
-        {
-            throw new System.ArgumentNullException(nameof(logger));
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Information, string.Format(CultureInfo.InvariantCulture, message, args));
     }
 
     public static void Debug(this BaseLogger? logger, string message, params object[] args)
     {
-        if (logger == null)
-        {
-            throw new System.ArgumentNullException(nameof(logger));
-        }
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
         logger.Log(LogLevel.Debug, string.Format(CultureInfo.InvariantCulture, message, args));
     }
 }
