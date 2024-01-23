f468af304133df124e13101c804b285c18d63e63
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-01-22 22:01:25 -0800

    HW2 hopefully done
    added ; to namespace and fixed variable names in FileLogger.cs
    
    Variable name fixes in BaseLoggerMixinsTests.cs
    
    Removed subtracting a second on FileLoggerTests.cs
    
    Added it in to work correctly on my machine
    
    Auto stash before rebase of "BMichaelis/Assignment2Test"

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Logger;
+
+public class FileLogger : BaseLogger
+{
+    private readonly string _Path;
+    public FileLogger(string filePath)
+    {
+        this._Path = filePath;
+    }
+    public override void Log(LogLevel LogLevel, String Message)
+    {
+        DateTime DateTime = DateTime.Now;
+        string ClassName = this.ClassName ?? "Wrong or Null name ?";
+        string message = $"{DateTime} {ClassName} {LogLevel} : {Message}";
+        File.AppendAllText(_Path, message + Environment.NewLine);
+    }
+}
