affd92dceccf84738923e0988a6e693e2f774fa8
EthanF <eflorko@ewu.edu>
2024-01-22 23:13:43 -0800

    Test Fixes

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,9 +1,35 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class FileLoggerTests
 {
+    [TestMethod]
+    public void Log_ValidInput_LogsMessageToFile()
+    {
+        //Arrange 
+        string filePath = "TestLogFile.txt";
+        var logger = new FileLogger(filePath);
 
+        // Act 
+        logger.Log(LogLevel.Information, "Test message");
+
+        //Assert
+        Assert.IsTrue(File.Exists(filePath));
+    }
+
+    [TestMethod]
+    public void Log_NullMessage_ThrowsException()
+    {
+        //Arrange
+        string filePath = "TestLogFile.txt";
+        var logger = new FileLogger(filePath);
+
+        //Act and Assert
+        Assert.ThrowsException<ArgumentNullException>(() => logger.Log(LogLevel.Information, null));
+
+    }
 }
--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -5,5 +5,31 @@ namespace Logger.Tests;
 [TestClass]
 public class LogFactoryTests
 {
+    [TestMethod]
+    public void CreateLogger_WithConfiguredFileLogger_ReturnsFileLoggerInstance()
+    {
+        var logger = new LogFactory();
+        string filePath = "TestLogFile.txt";
+        logFactory.ConfigureFileLogger(filePath);
 
+        //Act
+        var logger = LogFactory.CreateLogger("ExampleClass");
+        
+        //Assert
+        Assert.IsNotNull(logger);
+        Assert.IsInstanceOfType(logger, typeof(FileLogger));
+    }
+    
+    [TestMethod]
+    public void CreateLogger_WithoutConfiguredFileLogger_ReturnsNull()
+    {
+        //Arrange
+        var logFactory = new LogFactory();
+
+        //Act
+        var logger = logFactory.CreateLogger("ExampleClass");
+
+        //Asssert
+        Assert.IsNull(logger);
+    }
 }
