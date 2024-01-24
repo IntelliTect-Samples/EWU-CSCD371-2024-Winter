c5ae9cee4a83d54954ae8e87f69a1c2e21e1e0bb
Robert Garcia <rgarcia28@ewu.edu>
2024-01-23 21:00:02 -0800

    added a new test to LogFactoryTests.cs

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -12,4 +12,15 @@ public class LogFactoryTests
         var logFactory = new LogFactory();
         Assert.ThrowsException<ArgumentNullException>(() => logFactory.Configure(null!));
     }
+
+    [TestMethod]
+    public void Configure_ValidPath_SetsLogFileLocation()
+    {
+        var logFactory = new LogFactory();
+
+        string validPath = "LogFile.txt";
+        string res = logFactory.Configure(validPath);
+        Assert.AreEqual(validPath, res);
+    }
+
 }
