f7841ee071493f6d99496b9e085a4d8e953a53e9
Ethan Guerin <eguerin@ewu.edu>
2024-01-24 14:36:47 -0800

    Assignment2 - fix: Pragma

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿#pragma warning disable IDE0056
+#pragma warning restore IDE0056
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 
