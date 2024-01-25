894e9d2c75d3405a0dc9f537653173e443c9cfce
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-01-24 20:23:39 -0800

    feat: started ec by adding Log_ValidMessage_WritesToConsoleSuccssfully failing test

---
+++ Logger.Tests/ConsoleLoggerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests;
+
+    [TestClass]
+    public class ConsoleLoggerTests{
+
+    public StringWriter? MockConsole { get; set; }
+
+    [TestInitialize]
+    public void SetNewConsoleOut()
+    {
+        MockConsole = new StringWriter();
+        Console.SetOut(MockConsole);
+    }
+
+
+    [TestMethod]
+    [DataRow("FileLoggerTests Warning: Test message", LogLevel.Warning, "Test message")]
+    public void Log_ValidMessage_WritesToConsoleSuccssfully(string expected, LogLevel level, string input)
+    {
+        ConsoleLogger logger = new();
+        logger.Log(level, input);
+
+        Assert.Equals(expected, MockConsole!.ToString());
+
+
+    }
+
+}
+
---
+++ Logger/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logger.Tests;
+
+    public class ConsoleLogger : BaseLogger
+    {
+        public ConsoleLogger()
+        {
+        }
+
+    public override void Log(LogLevel level, string message)
+    {
+        throw new NotImplementedException();
+    }
+}
