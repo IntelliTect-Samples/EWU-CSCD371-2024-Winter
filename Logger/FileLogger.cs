1c270644e6f4f6d8f61da3deaf8b9d87a171fc87
Purplepigs0129 <91215080+Purplepigs0129@users.noreply.github.com>
2024-01-24 22:11:36 -0800

    Update FileLogger.cs

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -12,11 +12,11 @@ public class FileLogger : BaseLogger
     {
         this._Path = filePath;
     }
-    public override void Log(LogLevel LogLevel, String Message)
+    public override void Log(LogLevel logLevel, String message)
     {
         DateTime DateTime = DateTime.Now;
         string ClassName = this.ClassName ?? "Wrong or Null name ?";
-        string message = $"{DateTime} {ClassName} {LogLevel} : {Message}";
-        File.AppendAllText(_Path, message + Environment.NewLine);
+        string MessageAppendedToFile = $"{DateTime} {ClassName} {logLevel} : {message}";
+        File.AppendAllText(_Path, MessageAppendedToFile + Environment.NewLine);
     }
 }
