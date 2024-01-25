ae4458ba064b28df636420adb0e8067ccbd67faf
Andree Ramirez <aramirez61@ewu.edu>
2024-01-24 19:34:49 -0800

    added using statement

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;  // Add this line
 
 namespace Logger.Tests;
 
@@ -6,22 +7,20 @@ namespace Logger.Tests;
 public class FileLoggerTests
 {
     [TestMethod]
-    public void log_Path_AppendToLine()
+    public void Log_Path_AppendToLine()
     {
         string fileName = @"C:\file.txt";
         FileLogger fileLogger = new FileLogger(fileName);
         fileLogger.Log(LogLevel.Warning, "Warnings");
-        string log;
+        string log = null;  // Initialize 'log' to null
 
-        using(StreamReader sr = File.OpenText(fileName))
+        using (StreamReader sr = File.OpenText(fileName))
         {
-            while (sr.EndOfStream)
+            while (!sr.EndOfStream)  // Change 'while (sr.EndOfStream)' to 'while (!sr.EndOfStream)'
             {
                 log = sr.ReadLine();
             }
         }
-        Assert.AreEqual($"{System.DateTime.Now} {"FileLoggerTests"} {LogLevel.Warning}: {"Warnings"}",  log);
+        Assert.AreEqual($"{System.DateTime.Now} {"FileLoggerTests"} {LogLevel.Warning}: {"Warnings"}", log);
     }
-    
-
 }
