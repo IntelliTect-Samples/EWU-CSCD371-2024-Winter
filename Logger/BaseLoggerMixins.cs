c40bad939aab4eaadece88fd08a73230e5894d6b
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-01-25 18:48:00 -0800

    fix: made BaseLoggerMixin methods extension methods

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -6,37 +6,32 @@ namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(BaseLogger? logger, string message, params string[] arguments)
+    public static void Error(this BaseLogger? logger, string message, params string[] arguments)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
+            ArgumentNullException.ThrowIfNull(logger,nameof(logger));
 
-            logger.Log(LogLevel.Error, string.Format(CultureInfo.InvariantCulture, message, arguments));
+            logger?.Log(LogLevel.Error, string.Format(CultureInfo.InvariantCulture, message, arguments));
 
     }
 
-    public static void Warning(BaseLogger? logger, string message, params string[] arguments)
+    public static void Warning(this BaseLogger? logger, string message, params string[] arguments)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
 
-
-            logger.Log(LogLevel.Warning, string.Format(CultureInfo.InvariantCulture, message, arguments));
+        logger.Log(LogLevel.Warning, string.Format(CultureInfo.InvariantCulture, message, arguments));
     }
 
-    public static void Information(BaseLogger? logger, string message, params string[] arguments)
+    public static void Information(this BaseLogger? logger, string message, params string[] arguments)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
 
-            logger.Log(LogLevel.Information, string.Format(CultureInfo.InvariantCulture, message, arguments));
+        logger.Log(LogLevel.Information, string.Format(CultureInfo.InvariantCulture, message, arguments));
      
     }
-    public static void Debug(BaseLogger? logger, string message, params string[] arguments)
+    public static void Debug(this BaseLogger? logger, string message, params string[] arguments)
     {
-        if (logger == null)
-            throw new ArgumentNullException(nameof(logger), "Logger is null");
+        ArgumentNullException.ThrowIfNull(logger, nameof(logger));
 
-            logger.Log(LogLevel.Debug, string.Format(CultureInfo.InvariantCulture, message, arguments));
+        logger.Log(LogLevel.Debug, string.Format(CultureInfo.InvariantCulture, message, arguments));
     }
 }
