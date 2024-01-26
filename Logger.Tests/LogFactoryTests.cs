99554395044c957c8ec607ac8bf1b1dea3345a92
Hadi <amallah@ewu.edu>
2024-01-25 21:24:01 -0800

    Semi colon

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,43 +1,42 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Logger.Tests
+namespace Logger.Tests;
+
+[TestClass]
+public class LogFactoryTests
 {
-    [TestClass]
-    public class LogFactoryTests
+    [TestMethod]
+    public void LogFactory_SetClassName_Fail()
     {
-        [TestMethod]
-        public void LogFactory_SetClassName_Fail()
-        {
-            LogFactory logFactory = new();
-            BaseLogger? logger = logFactory.CreateLogger("");
-            //if (logger != null)
-            //{
-            //    Assert.AreEqual("", logger.ClassName);
-            //}
-            //else
-            //{
-            //    Assert.Fail("Logger should not be null.");
-            //}
-            Assert.AreEqual(null, null);
-        }
-
-        [TestMethod]
-        public void LogFactory_SetClassName_Success()
-        {
-            LogFactory logFactory = new();
-            BaseLogger? logger = logFactory.CreateLogger("Logger");
-            //if (logger != null)
-            //{
-            //    Assert.AreEqual("Logger", logger.ClassName);
-            //}
-            //else
-            //{
-            //    Assert.Fail("Logger should not be null.");
-            //}
-
-            Assert.AreEqual("Logger", nameof(Logger));
-        }
-
-        
+        LogFactory logFactory = new();
+        BaseLogger? logger = logFactory.CreateLogger("");
+        //if (logger != null)
+        //{
+        //    Assert.AreEqual("", logger.ClassName);
+        //}
+        //else
+        //{
+        //    Assert.Fail("Logger should not be null.");
+        //}
+        Assert.AreEqual(null, null);
     }
+
+    [TestMethod]
+    public void LogFactory_SetClassName_Success()
+    {
+        LogFactory logFactory = new();
+        BaseLogger? logger = logFactory.CreateLogger("Logger");
+        //if (logger != null)
+        //{
+        //    Assert.AreEqual("Logger", logger.ClassName);
+        //}
+        //else
+        //{
+        //    Assert.Fail("Logger should not be null.");
+        //}
+
+        Assert.AreEqual("Logger", nameof(Logger));
+    }
+
+    
 }
