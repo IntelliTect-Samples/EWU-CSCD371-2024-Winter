0587501eadb01c8162886fe72138f5aa80401389
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-01-27 16:18:12 -0800

    new test for JokeService, new IJokeService file, new Moq depend

---
+++ CanHazFunny/CanHazFunny.Tests/IJokeServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace CanHazFunny.Tests;
+
+
+public class IJokeServiceTests
+{
+    [Fact]
+    public void GetJoke_RetriveAJocke_Success()
+    {
+        var mock = new Mock<IJokeService>();
+        mock.Setup(x => x.GetJoke()).Returns("Here is my joke :(");
+        IJokeService jokeService = mock.Object;
+        var result = new MockClass().GetJoke();
+        Assert.Equal(jokeService.GetJoke(), result);
+    }
+
+}
+
+public class MockClass : IJokeService
+{
+    public string GetJoke()
+    {
+        return "Here is my joke :)";
+    }
+}
