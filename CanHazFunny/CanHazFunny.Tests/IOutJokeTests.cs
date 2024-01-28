2ded06259f3f1baed6b3d859e0628bed90e4aa29
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-27 16:21:29 -0800

    renamed the mock class

--- CanHazFunny/CanHazFunny.Tests/IOutJokeTests.cs
+++ CanHazFunny/CanHazFunny.Tests/IOutJokeTests.cs
@@ -7,14 +7,14 @@ public class IOutJokeTests
     [Fact]
     public void TestName()
     {
-        MockClass mockClass = new();
+        MockOutJoke mockClass = new();
     
         string joke = mockClass.TellJoke(); 
         Assert.Equal("Why did the chicken cross the road? to get to the othet side", joke);   
     }
 }
 
-public class MockClass : IOutJoke
+public class MockOutJoke : IOutJoke
 {
     public string TellJoke(){
         return "Why did the chicken cross the road? to get to the othet side";
