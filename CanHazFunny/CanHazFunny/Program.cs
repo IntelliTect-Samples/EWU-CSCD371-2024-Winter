e5dbaf9f8b9dc54c7da5a768516cf8dfc47616fb
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-01-28 14:56:31 -0800

    fix: added in mstests again

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -2,14 +2,12 @@ namespace CanHazFunny;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
             //Feel free to use your own setup here - this is just provided as an example
             //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
 
              new Jester(new JokeService(), new OutputToScreen()).TellJoke();
         }
-    }
 }
