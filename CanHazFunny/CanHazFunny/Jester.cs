f0784123e24e294bf1ee73ce047e04290c034dc4
Andree Ramirez <aramirez61@ewu.edu>
2024-01-29 10:32:43 -0800

    added dependencies and constructor to Jester

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace CanHazFunny
 
 public class Jester : IJokeService, IJokeOutput
 {
+    private readonly IJokeService jokeService;
+    private readonly IJokeOutput jokeOutput;
+
+    public Jester(IJokeService jokeService, IJokeOutput jokeOutput)
+    {
+        this.jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+        this.jokeOutput = jokeOutput ?? throw new ArgumentNullException(nameof(jokeOutput));
+    }
     public string GetJoke()
     {
 
