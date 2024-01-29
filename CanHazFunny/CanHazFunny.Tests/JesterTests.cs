16858a6cc1b0814af7cc96a79399f74fd7bbdc39
Robert Garcia <rgarcia28@ewu.edu>
2024-01-28 20:17:07 -0800

    added two simple tests to the Jester.Tests.cs just to make sure Moq is working

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,9 +1,27 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Moq;
+using System;
 
 namespace CanHazFunny.Tests;
 
-[TestClass]
+
 public class JesterTests
 {
-    
+    [Fact]
+    public void IServiceProperty_SetNull_Successfully()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Jester(null!, new JokeOutput()));
+
+    }
+
+    [Fact]
+    public void IOutputProperty_SetNull_Successfully()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Jester(new JokeService(), null!));
+
+    }
+
 }
+
+
+
