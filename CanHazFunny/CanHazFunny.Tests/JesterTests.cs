e56994a7931000d75d306a6733fd1c8f0bf01d61
Robert Garcia <rgarcia28@ewu.edu>
2024-01-28 20:44:08 -0800

    added a new test to the JesterTests.cs class

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -8,17 +8,35 @@ namespace CanHazFunny.Tests;
 public class JesterTests
 {
     [Fact]
-    public void IServicePropertySetNullSuccessfully()
+    public void IServiceProperty_SetNull_Successfully()
     {
         Assert.Throws<ArgumentNullException>(() => new Jester(null!, new JokeOutput()));
 
     }
 
     [Fact]
-    public void IOutputPropertySetNullSuccessfully()
+    public void IOutputProperty_SetNull_Successfully()
     {
         Assert.Throws<ArgumentNullException>(() => new Jester(new JokeService(), null!));
 
+    }
+
+    [Fact]
+    public void TellJoke_ChuckNorrisJokeSkips_Successfully()
+    {
+        var mockService = new Mock<IService>();
+        var mockOutput = new Mock<IOutput>();
+
+        mockService.SetupSequence(x => x.GetJoke())
+                       .Returns("Chuck Norris joke")
+                       .Returns("LMAO");
+
+        var jester = new Jester(mockService.Object, mockOutput.Object);
+
+        jester.TellJoke();
+
+        mockService.Verify(x => x.GetJoke(), Times.Exactly(2));
+        mockOutput.Verify(x => x.WriteJoke("LMAO"), Times.Once);
 
     }
 
