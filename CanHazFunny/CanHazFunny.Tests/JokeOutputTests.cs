5b78619bdc5edd8b2515aae5ff8708c09d4911ba
Hadi <amallah@ewu.edu>
2024-01-29 12:54:37 -0800

    Testing out how to test
    LOL

--- CanHazFunny/CanHazFunny.Tests/JokeOutputTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JokeOutputTests.cs
@@ -1,8 +1,31 @@
+using System;
 using Xunit;
 using Moq;
+
 namespace CanHazFunny.Tests;
 
 public class JokeOutputTests
 {
-    [Fact]
+    
+    //public JokeOutputTests()
+    //{
+    //    JokeOutput jokePrint = new();
+    //}
+
+    [Theory]
+    [inlineData("JOKE1")]
+    [inlineData ("JOKE2")]
+    public void PrintingJoke_OutputToConsole_Success(string output)
+    {
+        JokeOutput jokePrint = new();
+        jokePrint.PrintingJoke(output);
+
+        //Mock mockConsole = new < IConsoleWrapper > ();
+        //string outputString =  mockConsole.WriteLine();
+
+    }
 }
+//public interface IConsoleWrapper
+//{
+//    void WriteLine(string value);
+//}
