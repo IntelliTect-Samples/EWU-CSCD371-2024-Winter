9e9db7aafef5d01dcbdfd11d24845670a4a07011
Purplepigs0129 <praythopson@ewu.edu>
2024-01-29 10:11:40 -0800

    Added more tests

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CanHazFunny.Tests;
+
+public class JokeServiceTests
+{
+    [Fact]
+    public void GetJoke_RetrievesJoke_ReturnsSucessful()
+    {
+        JokeService jokeService = new();
+        string joke = jokeService.GetJoke();
+        Assert.NotEmpty(joke);
+    }
+}
---
+++ CanHazFunny/CanHazFunny.Tests/JokeToScreenTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using System.Globalization;
+using System.IO;
+
+namespace CanHazFunny.Tests;
+
+public class JokeToScreenTests
+{
+    [Fact]
+    public void PrintJoke_OutputsJoke_Successful()
+    {
+        JokeToScreen jokeToScreen = new();
+        StringWriter stringWriter = new(CultureInfo.InvariantCulture);
+        Console.SetOut(stringWriter);
+        jokeToScreen.PrintJoke("Test Joke");
+        string consoleOutput = stringWriter!.ToString();
+
+        Assert.Equal("Test Joke" + Environment.NewLine, consoleOutput);
+    }
+
+    [Fact]
+    public void PrintJoke_OutputsNull_ThrowsException()
+    {
+        JokeToScreen jokeToScreen = new JokeToScreen();
+        Assert.Throws<ArgumentNullException>(() => jokeToScreen.PrintJoke(null!));
+    }
+}
