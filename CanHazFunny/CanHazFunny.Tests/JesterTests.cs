1576b927c4bf869214ac66f0165c2bce40b0091c
Ethan Guerin <eguerin@ewu.edu>
2024-01-29 09:29:53 -0800

    Assignment3 - fix: Accidentally deleted main

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,7 +1,59 @@
+using Moq;
+using System;
 using Xunit;
 
 namespace CanHazFunny.Tests;
 
 public class JesterTests
 {
+    private readonly Mock<IJokeService>? _jokeServiceMock;
+    private readonly Mock<IOutputService>? _mockConsoleOutput;
+    private readonly Jester _jester;
+
+    public JesterTests()
+    {
+        _jokeServiceMock = new Mock<IJokeService>();
+        _mockConsoleOutput = new Mock<IOutputService>();
+        _jester = new Jester(_jokeServiceMock.Object, _mockConsoleOutput.Object);
+    }
+
+    [Fact]
+    public void TellJoke_SkipChuckNorris_PrintNormal()
+    {
+        var chuckNorrisJoke = "Chuck Norris hehe";
+        var regularJoke = "Regular Joke";
+        var callCount = 0;
+        _jokeServiceMock?.Setup(js => js.GetJoke()).Returns(() => callCount++ == 0 ? chuckNorrisJoke : regularJoke);
+
+        _jester.TellJoke();
+
+        _mockConsoleOutput?.Verify(co => co.WriteJoke(regularJoke), Times.Once);
+
+    }
+
+    [Fact]
+    public void TellJoke_WritesNonChuckNorris_PrintNormal()
+    {
+        var regularJoke = "Regular hehe";
+        _jokeServiceMock?.Setup(js => js.GetJoke()).Returns(regularJoke);
+
+        _jester.TellJoke();
+
+        _mockConsoleOutput?.Verify(co => co.WriteJoke(regularJoke), Times.Once);
+    }
+
+    [Fact]
+    public void Jester_NullIJokeService_ThrowsException()
+    {
+        var exception = Assert.Throws<ArgumentNullException>(() => new Jester(null, _mockConsoleOutput.Object));
+        Assert.Equal("jokeService", exception.ParamName);
+    }
+
+    [Fact]
+    public void Jester_NullOutputService_ThrowsException()
+{
+        var exception = Assert.Throws<ArgumentNullException>(() => new Jester(_jokeServiceMock.Object, null));
+        Assert.Equal("outputService", exception?.ParamName);
+    
+}
 }
---
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanHazFunny;
+
+
+public class Jester(IJokeService? jokeService, IOutputService? outputService)
+{
+    private readonly IJokeService _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+    private readonly IOutputService _outputService = outputService ?? throw new ArgumentNullException(nameof(outputService));
+
+    public void TellJoke()
+    {
+        string joke;
+        do
+        {
+            joke = _jokeService.GetJoke();
+        } while (joke.Contains("Chuck Norris"));
+
+        _outputService.WriteJoke(joke);
+    }
+}
