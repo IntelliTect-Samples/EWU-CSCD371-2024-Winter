3491cf685041360f6a6601508975b2e824f3b636
EthanF <eflorko@ewu.edu>
2024-01-29 22:34:55 -0800

    Enabled nullability and all following enables
    enabled all as needed in description of assignment

--- CanHazFunny/CanHazFunny/Jester.cs
+++ CanHazFunny/CanHazFunny/Jester.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CanHazFunny
 {
-    internal class Class1
+    public class Jester
     {
+        private readonly IOutputWriter _outputWriter;
+        private readonly IJokeService _jokeService;
+        
+        //Constructor with DI
+        public Jester (IOutputWriter outputWriter, IJokeService jokeService)
+        {
+            //Null for dependencies
+            _outputWriter = outputWriter ?? throw new ArgumentNullException(nameof(outputWriter));
+            _jokeService = jokeService ?? throw new ArgumentNullException(nameof(jokeService));
+        }
+
+        public void TellJoke()
+        {
+            string joke = _jokeService.GetJoke();
+
+            while (joke.Contains("Chuck Norris"))
+            {
+                joke = _jokeService.GetJoke();
+            }
+
+            _outputWriter.Write(joke);
+        }
+    
     }
 }
