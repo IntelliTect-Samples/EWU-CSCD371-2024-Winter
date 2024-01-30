29e9e610391dbe702b3d8d9e4c1f8612132cc847
connerhanson <connerhanson0826@gmail.com>
2024-01-29 21:17:37 -0800

    fixed pull request build issues

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -5,7 +5,9 @@ public class Program
     public static void Main(string[] args)
     {
         //Feel free to use your own setup here - this is just provided as an example
-        Jester jester = new(new JokeService(), new JokeOutput());
+        JokeService jokeService = new();
+        JokeOutput jokeOutput = new();
+        Jester jester = new(jokeService, jokeOutput);
         jester.TellJoke();
     }
 }
