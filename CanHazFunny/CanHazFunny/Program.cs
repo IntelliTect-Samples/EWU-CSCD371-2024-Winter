f5316471e106187870bdcad16128d57227b4a023
connerhanson <connerhanson0826@gmail.com>
2024-01-29 21:11:50 -0800

    fixed pull request build errors

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace CanHazFunny;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         //Feel free to use your own setup here - this is just provided as an example
-        new Jester(new JokeService(), new JokeOutput()).TellJoke();
+        Jester jester = new(new JokeService(), new JokeOutput());
+        jester.TellJoke();
     }
 }
