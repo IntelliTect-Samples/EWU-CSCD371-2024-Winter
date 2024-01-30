19044a569d671b60c3a1e8bfc23b674753a3973d
EthanF <eflorko@ewu.edu>
2024-01-29 23:03:28 -0800

    Syntax fix
    syntax

--- CanHazFunny/CanHazFunny/JokeService.cs
+++ CanHazFunny/CanHazFunny/JokeService.cs
@@ -6,17 +6,19 @@ namespace CanHazFunny
     public class JokeService : IJokeService
     {
         private HttpClient HttpClient { get; } = new();
+
 =======
 namespace CanHazFunny;
 >>>>>>> d7110209c32aafd7f0d4bd877409d09bf9f50e1a
+        public class JokeService
+        {
+            private HttpClient HttpClient { get; } = new();
 
-public class JokeService
-{
-    private HttpClient HttpClient { get; } = new();
-
-    public string GetJoke()
-    {
-        string joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
-        return joke;
+            public string GetJoke()
+            {
+                string joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
+                return joke;
+            }
+        }
     }
 }
