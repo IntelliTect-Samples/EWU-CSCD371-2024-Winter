9b237e6dd9c7f48af4e547eb868a979a0723744b
EthanF <eflorko@ewu.edu>
2024-01-29 22:34:13 -0800

    Interface syntax fixes
    syntax fix

--- CanHazFunny/CanHazFunny/Interfaces/IJokeService.cs
+++ CanHazFunny/CanHazFunny/Interfaces/IJokeService.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CanHazFunny.Interfaces
+namespace CanHazFunny
 {
-    internal class Class1
+    public interface IJokeService
     {
+        string GetJoke();
     }
 }
