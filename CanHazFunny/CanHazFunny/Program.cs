6e9c6527fbec318b73db7d48527ad714ef224181
Nathan <nmarsee@ewu.edu>
2024-01-30 10:21:17 -0800

    Made Main() do something

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -4,7 +4,9 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        //Feel free to use your own setup here - this is just provided as an example
-        new Jester(new JokeOutput(), new JokeService()).TellJoke();
+        JokeService jokeService = new();
+        JokeOutput jokeOutput = new();
+        Jester jester = new(jokeOutput, jokeService);
+        jester.TellJoke();
     }
 }
