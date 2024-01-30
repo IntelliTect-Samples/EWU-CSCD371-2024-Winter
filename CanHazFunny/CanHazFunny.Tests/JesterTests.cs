d6675a6212eafb88ee79ff810717c6d25c4a89e8
EthanF <eflorko@ewu.edu>
2024-01-29 22:33:49 -0800

    Tests creation
    tests

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,10 +1,47 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CanHazFunny.Tests
 {
     [TestClass]
     public class JesterTests
     {
-        
+        [TestMethod]
+        public void TellJoke_Should_Write_To_Output()
+        {
+            var mockOutputWriter = new Mock <IOutputWriter>();
+            var mockJokeService = new Mock <IJokeService>();
+
+            mockJokeService.Setup(s => s.GetJoke()).Returns("Funny Joke");
+
+            //Instance of Jester with MD
+            var jester = new Jester(mockOutputWriter.Object, mockJokeService.Object);
+
+            //Act
+            jester.TellJoke();
+
+            //Assert
+            mockOutputWriter.Verify(o => o.Write(It.IsAny<string>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void TellJoke_Skip_Chuck_Norris_Jokes()
+        {
+            //Arrange 
+            var mockOutputWriter = new Mock <IOutputWriter>();
+            var mockJokeService = new Mock <IJokeService>();
+
+            mockJokeService.SetupSequence(s => s.GetJoke())
+                .Returns("Chuck Norris Joke")
+                .Returns("Funny Joke");
+
+            var jester = new Jester(mockOutputWriter.Object, mockJokeService.Object);
+
+            //Act
+            jester.TellJoke();
+
+            //Assert 
+            mockOutputWriter.Verify(o => o.Write(It.IsAny<string>()), Times.Once);
+        }
     }
 }
