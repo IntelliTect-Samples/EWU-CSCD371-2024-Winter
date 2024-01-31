6c504d3b4e9f035eb1a0a7142ed7bcc195b6c5e4
Nathan <nmarsee@ewu.edu>
2024-01-30 22:58:45 -0800

    Add simple tests for JokeOutput and JokeService

---
+++ CanHazFunny/CanHazFunny.Tests/JokeOutputTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace CanHazFunny.Tests;
+
+public class JokeOutputTests
+{
+    [Fact]
+    public void PrintJoke_GivenString_PrintsString()
+    {
+        JokeOutput jokeOutput = new();
+
+        using StringWriter output = new();
+        Console.SetOut(output);
+
+        jokeOutput.PrintJoke("Test joke");
+        Assert.Equal("Test joke", output.ToString().Trim());
+    }
+
+    [Fact]
+    public void PrintJoke_GivenNull_ThrowsException()
+    {
+        JokeOutput jokeOutput = new();
+
+        using StringWriter output = new();
+        Console.SetOut(output);
+
+        Assert.Throws<ArgumentNullException>(() => jokeOutput.PrintJoke(null!));
+    }
+}
---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace CanHazFunny.Tests;
+
+public class JokeServiceTests
+{
+    [Fact]
+    public void GetJoke_CallWebsite_ReturnJoke()
+    {
+        JokeService jokeService = new();
+        string result = jokeService.GetJoke();
+        Assert.NotNull(result);
+    }
+}
