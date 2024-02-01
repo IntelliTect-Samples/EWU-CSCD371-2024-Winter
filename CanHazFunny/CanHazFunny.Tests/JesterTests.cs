36a0933ad838c1dba2edcc1216b48933c716b33c
Andree Ramirez <aramirez61@ewu.edu>
2024-01-31 18:04:29 -0800

    test changes

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -2,31 +2,26 @@ using Xunit;
 using Moq;
 using System;
 
-namespace CanHazFunny.Tests;
-
-public class JesterTests
+namespace CanHazFunny.Tests
 {
+    public class JesterTests
+    {
         [Fact]
         public void Constructor_NullJokeService_ThrowsArgumentNullException()
         {
-        
-            IJokeService jokeService = null;
+            IJokeService? jokeService = null;  // Use nullable reference type annotation
             IJokeOutput jokeOutput = Mock.Of<IJokeOutput>();
 
-            
-            Assert.Throws<ArgumentNullException>(() => new Jester(jokeService, jokeOutput));
+            Assert.Throws<ArgumentNullException>(() => new Jester(jokeService!, jokeOutput));  // Use the null-forgiving operator (!)
         }
 
         [Fact]
         public void Constructor_NullJokeOutput_ThrowsArgumentNullException()
         {
-            
             IJokeService jokeService = Mock.Of<IJokeService>();
-            IJokeOutput jokeOutput = null;
+            IJokeOutput? jokeOutput = null;  // Use nullable reference type annotation
 
-            
-            Assert.Throws<ArgumentNullException>(() => new Jester(jokeService, jokeOutput));
+            Assert.Throws<ArgumentNullException>(() => new Jester(jokeService, jokeOutput!));  // Use the null-forgiving operator (!)
         }
-
-        
+    }
 }
