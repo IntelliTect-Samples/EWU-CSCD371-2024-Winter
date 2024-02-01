54c13fa02cd41c6863c0f78e7e67fd4285c9dcc6
Robert Garcia <rgarcia28@ewu.edu>
2024-02-01 15:24:18 -0800

    new test

--- CanHazFunny/CanHazFunny.Tests/JokeServiceTest.cs
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTest.cs
@@ -1,9 +1,11 @@
 using Xunit;
 using Moq;
 using System;
+using System.Globalization;
+using System.IO;
 
 namespace CanHazFunny.Tests;
-#pragma warning disable CA1707
+#pragma warning disable CA1707 // could not figure out my _ was giving me issues in the build 
 
 
 public class JokeServiceTest
@@ -20,6 +22,24 @@ public class JokeServiceTest
 
         // Arrange
         Assert.NotNull(joke);
+    }
+
+    [Theory]
+    [InlineData("Where do computer programmers go for fun? The disc-o!")]
+    [InlineData("What's a computer scientist's favourite type of coffee? Java.")]
+    public void WriteJokeToScreen_ValidJoke_PrintsCorrectly(string joke)
+    {
+        // Arrange
+        JokeOutput JokeWriter = new();
+        using StringWriter streamWriter = new(CultureInfo.InvariantCulture);
+        Console.SetOut(streamWriter);
+
+        // Act
+        JokeWriter.WriteJoke(joke);
+        string consoleOutput = streamWriter.ToString();
+
+        // Assert
+        Assert.Equal(joke + Environment.NewLine, consoleOutput);
     }
 
 }
