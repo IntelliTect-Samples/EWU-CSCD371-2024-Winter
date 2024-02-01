43134fda5d91fdc92695d3368afdf29203f56213
Joshua Merrell <jmerrell2@ewu.edu>
2024-02-01 15:32:33 -0800

    Add FullName record and tests

---
+++ Logger.Tests/FullNameTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Logger.Tests
+{
+    public class FullNameTests
+    {
+        [Theory]
+        [InlineData("First", "Last", "Middle", "First Middle Last")]
+        [InlineData("First", "Last", null, "First Last")]
+        public void FullName_ToString_HandlesNullMiddle(string first, string last, string? middle, string expects)
+        {
+            FullName name = new(first, last, middle);
+            Assert.Equal(expects, name.ToString());
+        }
+
+        [Fact]
+        public void FullName_ToString_HandlesOptionalMiddle()
+        {
+            FullName name = new("First", "Last");
+            Assert.Equal("First Last", name.ToString());
+        }
+    }
+}
---
+++ Logger/FullName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public record class FullName(string First, string Last, string? Middle = null)
+    {
+        public override string ToString()
+        {
+            return $"{First} {((Middle!=null)?Middle + " ":"")}{Last}";
+        }
+    }
+}
