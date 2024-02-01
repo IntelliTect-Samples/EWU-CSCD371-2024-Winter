6c406701c375fefd904761d751748d3c86a42359
Andree Ramirez <aramirez61@ewu.edu>
2024-01-31 18:29:15 -0800

    added chuck norris check test

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -5,6 +5,19 @@ using System;
 namespace CanHazFunny.Tests;
     public class JesterTests
     {
+
+        private readonly Mock<IJokeService> JokeServiceMock;
+        private readonly Mock<IJokeOutput> JokeOutputMock;
+        private readonly Jester _jesterInstance;
+
+        public JesterTests()
+        {
+            JokeServiceMock = new Mock<IJokeService>();
+            JokeOutputMock = new Mock<IJokeOutput>();
+            _jesterInstance = new Jester(JokeServiceMock.Object, JokeOutputMock.Object);
+        }
+
+
         [Fact]
         public void Constructor_NullJokeService_ThrowsArgumentNullException()
         {
@@ -22,5 +35,23 @@ namespace CanHazFunny.Tests;
 
             Assert.Throws<ArgumentNullException>(() => new Jester(jokeService, jokeOutput!));  // Use the null-forgiving operator (!)
         }
+
+       [Fact]   
+        public void TellJoke_ChuckNorrisJoke_Skips()
+        {
+            string chuckJoke = "Chuck Norris";
+            string joke = "joke";
+            int count = 0;
+
+            JokeServiceMock?.Setup(js => js.GetJoke()).Returns(() => count++ == 0 ? chuckJoke : joke);
+
+            _jesterInstance.TellJoke();
+
+            // Verify that the method was called with the non-Chuck Norris joke
+            JokeOutputMock?.Verify(co => co.PrintingJoke(joke), Times.Once);
+        }
+
+
+        
     }
 
