f1bf88bf9aa269f849afdcdbe4dab17f1a305f26
alyxadarr <adarrington@ewu.edu>
2024-02-01 14:55:11 -0800

    Created FullName Record and fields in FullName.cs

---
+++ Logger/FullName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public record FullName(string FirstName, string MiddleName, string LastName)
+    {
+        public string FirstName { get; } = FirstName ?? throw new ArgumentException(nameof(FirstName));
+        public string MiddleName { get; } = MiddleName ?? throw new ArgumentException(nameof(MiddleName));
+        public string LastName { get; } = LastName ?? throw new ArgumentException(nameof(LastName));
+
+    }
+}
