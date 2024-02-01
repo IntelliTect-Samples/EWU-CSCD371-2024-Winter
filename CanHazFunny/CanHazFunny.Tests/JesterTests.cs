6f9148ccbe2c0a497ad54f2257b5c93d95fb064a
Andree Ramirez <aramirez61@ewu.edu>
2024-01-31 17:58:19 -0800

    added null exception tests for jester

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,13 +1,32 @@
 using Xunit;
 using Moq;
+using System;
+
 namespace CanHazFunny.Tests;
 
 public class JesterTests
 {
-    [Fact]
-    public void TESTPASS()
-    {
-        string test = "expected";
-        Assert.Equal("expected", test);
-    }
+        [Fact]
+        public void Constructor_NullJokeService_ThrowsArgumentNullException()
+        {
+        
+            IJokeService jokeService = null;
+            IJokeOutput jokeOutput = Mock.Of<IJokeOutput>();
+
+            
+            Assert.Throws<ArgumentNullException>(() => new Jester(jokeService, jokeOutput));
+        }
+
+        [Fact]
+        public void Constructor_NullJokeOutput_ThrowsArgumentNullException()
+        {
+            
+            IJokeService jokeService = Mock.Of<IJokeService>();
+            IJokeOutput jokeOutput = null;
+
+            
+            Assert.Throws<ArgumentNullException>(() => new Jester(jokeService, jokeOutput));
+        }
+
+        
 }
