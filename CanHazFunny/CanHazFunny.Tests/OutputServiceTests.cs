778a829df0d4b5a14d0fa13a6fe08acc64d19ce3
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-01 16:51:37 -0800

    refactor: OutputServiceTests are refactored based on the peer review

--- CanHazFunny/CanHazFunny.Tests/OutputServiceTests.cs
+++ CanHazFunny/CanHazFunny.Tests/OutputServiceTests.cs
@@ -6,21 +6,6 @@ namespace CanHazFunny.Tests;
 
 public class OutputServiceTests
 {
-    [Fact]
-    public void DisplayJoke_ThrowsArgumentNull_Success()
-    {
-        OutputService displayJoke = new();
-        Assert.Throws<ArgumentNullException>(() => displayJoke.DisplayJoke(null!));
-    }
-
-    [Fact]
-    public void DisplayJoke_JokeDisplays_Success()
-    {
-        MockOutput mockClass = new();
-        mockClass.DisplayJoke("knock knock joke");
-
-        Assert.Equal("knock knock joke", mockClass.GetJoke());
-    }
 
     [Fact]
     public void DisplayJoke_ValidJoke_WritesToConsole()
@@ -36,20 +21,5 @@ public class OutputServiceTests
 
         // Assert
         Assert.Contains(joke, stringWriter.ToString());
-    }
-}
-
-public class MockOutput : IOutputService
-{
-    private string? Joke { get; set; }
-
-    public void DisplayJoke(string joke)
-    {
-        Joke = joke;
-    }
-
-    public string GetJoke()
-    {
-        return Joke!;
     }
 }
