9a0a5bebca676690125d19b6bfc8b7cf8c975617
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-01 16:31:36 -0800

    test: New JokeServiceTests added

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CanHazFunny.Tests
+{
+    public class JokeServiceTests
+    {
+        [Fact]
+        public void GetJoke_ReciveString_Success()
+        {
+            JokeService service = new JokeService();
+            Assert.IsType<string>(service.GetJoke());
+        }
+    }
+}
