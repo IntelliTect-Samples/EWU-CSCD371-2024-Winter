94fef424040a740842ab13aded5633cb4f0b4209
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-03 15:20:58 -0800

    tests: added in tests for null checking first and last name

--- Logger.Tests/FullNameTests.cs
+++ Logger.Tests/FullNameTests.cs
@@ -1,13 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
-namespace Logger.Tests
-{
-    internal class FullNameTests
+namespace Logger.Tests;
+
+    
+    public class FullNameTests
     {
+        [Fact]
+        public void FullName_NullFirstName_ThrowsArgumentNullException()
+        {
+        //Arrange
+        #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        Assert.Throws<ArgumentNullException>(() => new FullName(null, "Johnson"));
+        #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        }
 
+        [Fact]
+        public void FullName_NullLastName_ThrowsArgumentNullException()
+        {
+        //Arrange
+        #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        Assert.Throws<ArgumentNullException>(() => new FullName("Jake", null));
+        #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        }
     }
-}
+
--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Xml.Linq;
-
+﻿
 namespace Logger
 {
-    public record FullName(string FirstName, string MiddleName, string LastName)
+    public record class FullName(string FirstName, string LastName, string MiddleName = "")
     {
        // Provide a comment on the full name record on why you selected to define a value or a reference type and ❌✔
        //Provide a comment on the full name record on why or why not the type is immutable.
-        public string FirstName { get; } = FirstName ?? throw new ArgumentException(nameof(FirstName));
-        public string MiddleName { get; } = MiddleName ?? throw new ArgumentException(nameof(MiddleName));
-        public string LastName { get; } = LastName ?? throw new ArgumentException(nameof(LastName));
+        public string FirstName { get; } = FirstName ?? throw new ArgumentNullException(nameof(FirstName));
+        public string MiddleName { get; } = MiddleName;
+        public string LastName { get; } = LastName ?? throw new ArgumentNullException(nameof(LastName));
 
     }
 }
