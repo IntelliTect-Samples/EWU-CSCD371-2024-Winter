e0072893ae27d0a624a8b7e92b7a309c71a83de2
Purplepigs0129 <praythopson@ewu.edu>
2024-02-03 10:08:44 -0800

    setup some files to be used for the assignment

---
+++ Logger/FullName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public record class FullName (
+        string firstName, string lastName, string middleName);
+    //comments needed as well as null and optional
+    //could be combined in one file if we want
+    //also needs to be setup
+}
