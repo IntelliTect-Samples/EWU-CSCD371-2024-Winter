0792467fe5d90f3c73aebafc47201dc00a6e06d1
Paul <paguilar4@ewu.edu>
2024-02-04 14:32:02 -0800

    Created FullName record
    i think

---
+++ Logger/FullName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger {
+
+    public readonly record struct FullNameRecord
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string? MiddleName { get; }
+
+        public FullNameRecord(string firstName, string lastName, string? middleName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName;
+        }
+
+    }
+}
