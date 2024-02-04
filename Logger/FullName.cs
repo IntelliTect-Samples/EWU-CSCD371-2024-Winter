97dc4cea22d20b4c1dd743bcb28d25d06600d8c7
Ethan Guerin <eguerin@ewu.edu>
2024-02-04 15:41:49 -0800

    Assignment4 - fix/add: Added BaseEntity and Book, Chose Record for FName
    Also fixed some .csproj things.

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logger
+namespace Logger;
+
+public record FullName(string FirstName, string? MiddleName, string LastName)
 {
-    internal class Class1
-    {
-    }
+    // We chose record because it is both Immutable and it has valuie equality behavior.
+    // Records are immutable because they cannot be changed once created.
 }
