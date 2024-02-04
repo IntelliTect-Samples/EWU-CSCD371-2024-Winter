1033c17f13f0e14cd9ad02d28ec8e9269ae7d8f5
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-03 20:02:48 -0800

    added employee enity tests and starting files

---
+++ Logger.Tests/EmployeeTests.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Xunit;
+
+namespace Logger.Tests;
+
+    public class EmployeeTests
+    {
+    #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+
+    [Fact]
+    public void Employee_NullFirstname_ThrowsNullPointerException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Employee(null, "Davis", 56000));
+    }
+
+    [Fact]
+    public void Employee_NullLastName_ThrowsNullPointerException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Employee("John", null, 56000));
+    }
+
+    [Fact]
+    public void ToString_ValidStudent_ReturnsFormattedName()
+    {
+        var employee = new Employee("John", "Davis", 1345);
+
+        Assert.Equal("John Davis, Salary: 56000", employee.Name);
+    }
+}
+
---
+++ Logger/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace Logger.Tests
+{
+    internal class Employee
+    {
+        private string v1;
+        private string v2;
+        private int v3;
+
+        public Employee(string v1, string v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
+}
