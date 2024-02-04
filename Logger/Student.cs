1922bd3b99492decfdd79d91add082c3a0f65ace
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-04 11:59:37 -0800

    added in Directory.Build.props

--- Logger/Student.cs
+++ Logger/Student.cs
@@ -4,10 +4,10 @@ namespace Logger;
 public record class Student(string FirstName, string LastName) : Person(FirstName, LastName), IEntity
 {
 
-    public Guid Id { get; init; }
-
     //Name member of IEntity should explicit becuase it has collision with Employee's name
     string IEntity.Name {get => $"{nameof(Student)}: {Name}"; }
+
+    public Guid Id { get; init; }
 
     public override string ToString() => base.ToString();
 
