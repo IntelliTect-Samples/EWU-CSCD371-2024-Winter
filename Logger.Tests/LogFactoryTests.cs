0e13fcbc6f7c2f5a562317cdcc83da74ff1e1139
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-04 18:28:18 -0800

    added in TODO comments in LogFactoryTests

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -1,13 +1,16 @@
 ﻿using Xunit;
 
 namespace Logger.Tests;
-
 public class LogFactoryTests : FileLoggerTestsBase
 {
+    // TODO: This test is incomplete. It should then call createLogger method and then assert that the BaseLogger
+    // Returned is not null and also that it's logSourse matches LogFactoryTests and FilePath is correct
     [Fact]
     public void ConfigureFileLogger_GivenFilePath_ReturnsFileLoggerWithSetFilePath()
     {
         LogFactory logFactory = new();
         logFactory.ConfigureFileLogger(FilePath);
     }
+
+    //TODO: Missing test for if ConfigureFileLogger isn't set which would return a null value
 }
