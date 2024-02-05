056d7464ce3d217260b5a4c9aaaf363c254054d0
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-04 18:21:26 -0800

    add in TODO comment in LogFactory

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,5 +1,7 @@
 ﻿namespace Logger;
-
+//TODO: I don't know if this is by design but because of FileLogger's constructor is public
+//but its using it create a FileLogger isntead of using FileLogger's own create logger method.
+//Maybe storing a Configuration proprety and then saving ti could be better for creating loggers
 public class LogFactory
 {
     public string? FileName { get; set; }
