314477f7a1a0e04a43eb0a4d68a264f377e29e4a
Trevor Felchlin <tfelchlin@ewu.edu>
2024-02-05 10:16:49 -0800

    Added IEntity implantation and BaseEnity class and tests

---
+++ Logger.Tests/BaseEntityTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Logger.Tests;
+public class BaseEntityTests{
+    private class TestEntity(string name) : BaseEntity
+    {
+        private string _name = name;
+
+        public override string GetName()
+        { 
+            return _name; 
+        }
+        protected override void SetName(string name)
+        {
+            _name = name;
+        }
+
+    }
+    [Fact]
+    public void BaseEntity_ShouldImplementIEntityInterfaceImplicitly_SuccessfulSet()
+    {
+        TestEntity entity = new ("name");
+
+        Assert.IsAssignableFrom<IEntity>(entity);
+    }
+    [Fact]
+    public void RetrieveName_Name_Successful()
+    {
+        TestEntity entity = new("name");
+
+        Assert.Equal("Name", entity.GetName());
+    }
+}
+
---
+++ Logger/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger;
+public abstract class BaseEntity : IEntity
+{
+    public Guid Id { get; init; }
+    
+    public string name
+    {
+        get => GetName();
+        set => SetName(value);
+    }
+
+    public abstract string GetName();
+    //I feel this may need to be protected not 100 percent sure
+    protected abstract void SetName(string name);
+}
+
+
