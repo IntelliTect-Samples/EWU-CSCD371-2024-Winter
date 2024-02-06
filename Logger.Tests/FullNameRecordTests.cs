34dc2907f50d021db002a3a1dfef794adcace3cd
Andree Ramirez <aramirez61@ewu.edu>
2024-02-06 11:31:18 -0800

    make sure middle name is null with 2 params test

--- Logger.Tests/FullNameRecordTests.cs
+++ Logger.Tests/FullNameRecordTests.cs
@@ -5,18 +5,29 @@ namespace Logger.Tests;
 public class FullNameRecordTests
 {
     [Fact]
-        public void FullNameRecord_WithMiddleName_ShouldSetPropertiesCorrectly()
-        {
-            string firstName = "John";
-            string lastName = "Doe";
-            string middleName = "Smith";
+    public void FullNameRecord_WithMiddleName_ShouldSetPropertiesCorrectly()
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+        string middleName = "Smith";
 
-            var fullNameRecord = new FullNameRecord(firstName, lastName, middleName);
+        var fullNameRecord = new FullNameRecord(firstName, lastName, middleName);
 
-            Assert.Equal(firstName, fullNameRecord.FirstName);
-            Assert.Equal(lastName, fullNameRecord.LastName);
-            Assert.Equal(middleName, fullNameRecord.MiddleName);
-        }
+        Assert.Equal(firstName, fullNameRecord.FirstName);
+        Assert.Equal(lastName, fullNameRecord.LastName);
+        Assert.Equal(middleName, fullNameRecord.MiddleName);
+    }
+
+    [Fact]
+    public void FullNameRecord_WithoutMiddleName_ShouldSetMiddleNameToNull()
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+
+        var fullNameRecord = new FullNameRecord(firstName, lastName);
+
+        Assert.Null(fullNameRecord.MiddleName);
+    }
 
 
 }
