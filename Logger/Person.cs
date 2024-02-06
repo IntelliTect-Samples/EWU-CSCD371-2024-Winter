4f5d8cd142f977960708a3e62e6ce15c98353b3f
EthanF <eflorko@ewu.edu>
2024-02-05 22:31:26 -0800

    Syntax fix for Person.cs which refactored Student and Employee

--- Logger/Person.cs
+++ Logger/Person.cs
@@ -4,26 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logger;
-
-public abstract class Person : BaseEntity
+namespace Logger
 {
-    public Name FullName { get; }
-
-    protected Person(Guid id, Name fullName)
+    public abstract class Person : BaseEntity
     {
-        Id = id;
-        FullName = fullName;
+        public Name FullName { get; }
+
+        protected Person(Guid id, Name fullName)
+        {
+            Id = id;
+            FullName = fullName;
+        }
     }
-}
 
-public record Student(Guid Id, Name FullName) : Person(Id, FullName)
-{
-    public override string Name => $"{FullName.FirstName} {FullName.MiddleName} {FullName.LastName}";
-}
+    public record Student(Guid Id, Name FullName) : Person(Id, FullName)
+    {
+        public override string Name => $"{FullName.FirstName} {FullName.MiddleName} {FullName.LastName}";
+    }
 
-public record Employee(Guid Id, Name FullName) : Person(Id, FullName)
-{
-    public override string Name => $"{FullName.FirstName} {FullName.MiddleName} {FullName.LastName}";
-}
-
+    public record Employee(Guid Id, Name FullName) : Person(Id, FullName)
+    {
+        public override string Name => $"{FullName.FirstName} {FullName.MiddleName} {FullName.LastName}";
+    }
+    }
