27036914f60da9fb9245c82908c7b45cce696a2c
EthanF <eflorko@ewu.edu>
2024-02-05 22:30:58 -0800

    Fixed repeated Tests | Added needed Tests

--- Logger.Tests/EntityTests.cs
+++ Logger.Tests/EntityTests.cs
@@ -32,6 +32,7 @@ public class EntityTests
         Exception ex = Assert.Throws<ArgumentException>(() => new Book(id, title));
     }
 
+
     [Fact]
     public void Student_Creation_Success()
     {
@@ -40,7 +41,7 @@ public class EntityTests
         string middleName = "Alexander";
         string lastName = "Guerin";
 
-        Person student = new(id, Name.Create(firstName, middleName, lastName));
+        Student student = new(id, Name.Create(firstName, middleName, lastName));
 
         Assert.Equal(firstName, student.FullName.FirstName);
         Assert.Equal(middleName, student.FullName.MiddleName);
@@ -56,34 +57,7 @@ public class EntityTests
     public void Student_InvalidData_ThrowError(string firstName, string? middleName, string lastName)
     {
         Guid id = Guid.NewGuid();
-        Exception ex = Assert.Throws<ArgumentException>(() => new Person(id, Name.Create(firstName, middleName, lastName)));
-    }
-
-    [Fact]
-    public void Employee_Creation_Success()
-    {
-        Guid id = Guid.NewGuid();
-        string firstName = "Ethan";
-        string middleName = "Alexander";
-        string lastName = "Guerin";
-
-        Employee employee = new(id, Name.Create(firstName, middleName, lastName));
-
-        Assert.Equal(firstName, employee.FullName.FirstName);
-        Assert.Equal(middleName, employee.FullName.MiddleName);
-        Assert.Equal(lastName, employee.FullName.LastName);
-        Assert.Equal(id, employee.Id);
-    }
-
-    [Theory]
-    [InlineData(null, "Alexander", "Guerin")]
-    [InlineData("Ethan", "Alexander", null)]
-    [InlineData("", "Alexander", "Guerin")]
-    [InlineData("Ethan", "Alexander", "")]
-    public void Employee_InvalidData_ThrowsError(string firstName, string? middleName, string lastName)
-    {
-        Guid id = Guid.NewGuid();
-        Exception ex = Assert.Throws<ArgumentException>(() => new Employee(id, Name.Create(firstName, middleName, lastName)));
+        Exception ex = Assert.Throws<ArgumentException>(() => new Student(id, Name.Create(firstName, middleName, lastName)));
     }
 
     [Fact]
@@ -120,8 +94,8 @@ public class EntityTests
     {
         Guid id = Guid.NewGuid();
         Name fullName = new("Ethan", "Alexander", "Guerin");
-        Person st1 = new(id, fullName);
-        Person st2 = new(id, fullName);    
+        Student st1 = new(id, fullName);
+        Student st2 = new(id, fullName);
 
         Assert.Equal(st1, st2);
     }
@@ -129,8 +103,8 @@ public class EntityTests
     [Fact]
     public void Student_DifferentIdandName_AreNotEqual()
     {
-        Person st1 = new(Guid.NewGuid(), new Name("Ethan", null, "Guerin"));
-        Person st2 = new(Guid.NewGuid(), new Name("Ethan", null, "Florko"));
+        Student st1 = new(Guid.NewGuid(), new Name("Ethan", null, "Guerin"));
+        Student st2 = new(Guid.NewGuid(), new Name("Ethan", null, "Florko"));
 
         Assert.NotEqual(st1, st2);
         Assert.NotEqual(st1.FullName, st2.FullName);
@@ -167,6 +141,56 @@ public class EntityTests
         Book book2 = new(id, "Happy Day");
 
         Assert.Equal(book1, book2);
+    }
+
+    [Fact]
+    public void Student_EqualStudents_AreEqual()
+    {
+        Guid id = Guid.NewGuid();
+        Name fullName = new("Ethan", "Alexander", "Guerin");
+        Student st1 = new(id, fullName);
+        Student st2 = new(id, fullName);
+
+        Assert.Equal(st1, st2);
+    }
+
+    [Fact]
+    public void Employee_EqualEmployees_AreEqual()
+    {
+        Guid id = Guid.NewGuid();
+        Name fullName = new("Ethan", "Alexander", "Guerin");
+        Employee st1 = new(id, fullName);
+        Employee st2 = new(id, fullName);
+
+        Assert.Equal(st1, st2);
+    }
+
+    [Fact]
+    public void Employee_Creation_Success()
+    {
+        Guid id = Guid.NewGuid();
+        string firstName = "Ethan";
+        string middleName = "Alexander";
+        string lastName = "Guerin";
+
+        Employee employee = new(id, Name.Create(firstName, middleName, lastName));
+
+        Assert.Equal(firstName, employee.FullName.FirstName);
+        Assert.Equal(middleName, employee.FullName.MiddleName);
+        Assert.Equal(lastName, employee.FullName.LastName);
+        Assert.Equal(id, employee.Id);
+    }
+
+    [Theory]
+    [InlineData(null, "Alexander", "Guerin")]
+    [InlineData("Ethan", "Alexander", null)]
+    [InlineData("", "Alexander", "Guerin")]
+    [InlineData("Ethan", "Alexander", "")]
+
+    public void Employee_InvalidData_ThrowsError(string firstName, string? middleName, string lastName)
+    {
+        Guid id = Guid.NewGuid();
+        Exception ex = Assert.Throws<ArgumentException>(() => new Employee(id, Name.Create(firstName, middleName, lastName)));
     }
 
 }
