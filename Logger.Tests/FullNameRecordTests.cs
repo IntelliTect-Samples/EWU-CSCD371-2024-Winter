5d848e51ea946b14a6741d31eb8e10daeefb6c00
Andree Ramirez <aramirez61@ewu.edu>
2024-02-06 11:25:41 -0800

    FullName record setting properties correctly test

--- Logger.Tests/FullNameRecordTests.cs
+++ Logger.Tests/FullNameRecordTests.cs
@@ -4,7 +4,19 @@ namespace Logger.Tests;
 
 public class FullNameRecordTests
 {
+    [Fact]
+        public void FullNameRecord_WithMiddleName_ShouldSetPropertiesCorrectly()
+        {
+            string firstName = "John";
+            string lastName = "Doe";
+            string middleName = "Smith";
 
+            var fullNameRecord = new FullNameRecord(firstName, lastName, middleName);
+
+            Assert.Equal(firstName, fullNameRecord.FirstName);
+            Assert.Equal(lastName, fullNameRecord.LastName);
+            Assert.Equal(middleName, fullNameRecord.MiddleName);
+        }
 
 
 }
