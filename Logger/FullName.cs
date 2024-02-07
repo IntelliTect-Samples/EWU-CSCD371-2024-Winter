d49d3ae8b50a56b89b0fd7e97412169f58169ecf
tfelchlin13 <tfelchlin@ewu.edu>
2024-02-06 16:03:35 -0800

    Fixed namespace issue in FullName

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logger
+namespace Logger;
+
+public record class FullName(string First, string Last, string? Middle = null)
 {
-    public record class FullName(string First, string Last, string? Middle = null)
+    public void Deconstruct(out string First, out string Last, out string? Middle)
     {
-        public void Deconstruct(out string First, out string Last, out string? Middle)
-        {
-            First = this.First;
-            Last = this.Last;
-            Middle = this.Middle;
-        }
-        public override string ToString()
-        {
-            return $"{First} {((Middle!=null)?Middle + " ":"")}{Last}";
-        }
+        First = this.First;
+        Last = this.Last;
+        Middle = this.Middle;
+    }
+    public override string ToString()
+    {
+        return $"{First} {((Middle!=null)?Middle + " ":"")}{Last}";
     }
 }
+
