7bc52f39c6bcbc362b50fd363d2afabd6a8eb85a
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-06 23:17:05 -0800

    added note for ILogger generic CreateLogger message

--- Logger/ILogger.cs
+++ Logger/ILogger.cs
@@ -8,6 +8,8 @@ public interface ILogger<T> where T : BaseLogger
     // While interesting, this is probably better implemented using a factory class.
     // because you can't have static abstract members on classes
     // and you can't have covariant return types on interface members. :(
+    //NOTE: This is the EC attempt but it is not correct. We have left it here to see if
+    //any other groups has done their own implmentation that works.
 #pragma warning disable CA1000
     static abstract T CreateLogger<T2>(in T2 configuration) where T2 : ILoggerConfiguration;
 #pragma warning restore CA1000
