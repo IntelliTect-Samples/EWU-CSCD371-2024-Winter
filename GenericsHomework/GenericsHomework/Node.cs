9e8966d9b758560a3682ae2b33009055d7b2f5c4
Alexa <adarrington@ewu.edu>
2024-02-07 11:39:28 -0800

    Created Node Class

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericsHomework
+{
+    public class Node<T>
+    {
+        public T value { get; }
+        public Node<T> next { get; private set; }
+        public Node(T value, Node<T> next)
+        {
+        }
+    }
+}
