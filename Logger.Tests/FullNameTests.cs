f1508570ff469e68a8401385f7bc3ca69e6aaee4
Trevor Felchlin <tfelchlin@ewu.edu>
2024-02-06 19:24:25 -0800

    Fix namespace in TestRecords and FullNameTests

--- Logger.Tests/FullNameTests.cs
+++ Logger.Tests/FullNameTests.cs
@@ -1,35 +1,35 @@
 ﻿using Xunit;
 
-namespace Logger.Tests
+namespace Logger.Tests;
+
+public class FullNameTests
 {
-    public class FullNameTests
+    [Theory]
+    [InlineData("First", "Last", "Middle", "First Middle Last")]
+    [InlineData("First", "Last", null, "First Last")]
+    public void FullName_ToString_HandlesNullMiddle(string first, string last, string? middle, string expects)
     {
-        [Theory]
-        [InlineData("First", "Last", "Middle", "First Middle Last")]
-        [InlineData("First", "Last", null, "First Last")]
-        public void FullName_ToString_HandlesNullMiddle(string first, string last, string? middle, string expects)
-        {
-            FullName name = new(first, last, middle);
-            Assert.Equal(expects, name.ToString());
-        }
+        FullName name = new(first, last, middle);
+        Assert.Equal(expects, name.ToString());
+    }
 
-        [Fact]
-        public void FullName_ToString_HandlesOptionalMiddle()
-        {
-            FullName name = new("First", "Last");
-            Assert.Equal("First Last", name.ToString());
-        }
+    [Fact]
+    public void FullName_ToString_HandlesOptionalMiddle()
+    {
+        FullName name = new("First", "Last");
+        Assert.Equal("First Last", name.ToString());
+    }
 
-        [Theory]
-        [InlineData("First", "Last", "Middle")]
-        [InlineData("First", "Last", null)]
-        public void FullName_Deconstruct(string first, string last, string? middle)
-        {
-            FullName name = new(first, last, middle);
-            (string firstDeconst, string lastDeconst, string? middleDeconst) = name;
-            Assert.Equal(first, firstDeconst);
-            Assert.Equal(last, lastDeconst);
-            Assert.Equal(middle, middleDeconst);
-        }
+    [Theory]
+    [InlineData("First", "Last", "Middle")]
+    [InlineData("First", "Last", null)]
+    public void FullName_Deconstruct(string first, string last, string? middle)
+    {
+        FullName name = new(first, last, middle);
+        (string firstDeconst, string lastDeconst, string? middleDeconst) = name;
+        Assert.Equal(first, firstDeconst);
+        Assert.Equal(last, lastDeconst);
+        Assert.Equal(middle, middleDeconst);
     }
 }
+
