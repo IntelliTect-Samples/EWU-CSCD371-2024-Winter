f792dfe3288cbf9f7c6eac89f2e29b4f738d1b95
Andree Ramirez <aramirez61@ewu.edu>
2024-02-08 10:26:32 -0800

    made student constructor test

--- Logger.Tests/StudentTests.cs
+++ Logger.Tests/StudentTests.cs
@@ -4,6 +4,18 @@ namespace Logger.Tests;
 
 public class StudentTests {
 
+    [Fact]
+    public void Student_ValidName_ConstructsWithFullName()
+    {
+        var fullName = new FullNameRecord("John", "Doe");
+     
+        var student = new StudentRecord(fullName);
+       
+        Assert.NotNull(student);
+        Assert.Equal("John Doe", student.Name);
+    }
+
+
 
     
 
