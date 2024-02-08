bdfe281bc52277d74be6edb74c82e110c5b796d0
Purplepigs0129 <praythopson@ewu.edu>
2024-02-08 14:46:46 -0800

    fix more errors on FullName

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -5,29 +5,17 @@ namespace Logger;
 // I've changed this to a value type since it is more or less just a simple variable, the entities that
 // FullName will apply to should handle the uniqueness of the object.
 
-// I think FullName should be a mutable type? Since people can change their name. 
+// FullName is a immutable type due to the fact of readonly
 
-public readonly record struct FullName
+public readonly record struct FullName(string firstName, string lastName, string? middleName = "")
 {
-    public string FirstName {  get; }
-    public string? MiddleName { get; } = null;
-    public string LastName { get; }
-
-    public FullName(string firstName, string? middleName, string lastName)
-    {
-        ArgumentException.ThrowIfNullOrWhiteSpace(firstName);
-        ArgumentException.ThrowIfNullOrWhiteSpace(lastName);
-        //middle can be null as defualt since some people dont have one, thus it should be the only nullable
-
-        FirstName = firstName;
-        MiddleName = middleName;
-        LastName = lastName;
-    }
+    public string FirstName { get; } = firstName ?? throw new ArgumentNullException(nameof(FirstName));
+    public string MiddleName { get; } = middleName ?? "";
+    public string LastName { get; } = lastName ?? throw new ArgumentNullException(nameof(LastName));
 
     public override string ToString()
-    {   //we definetly talked about a more effcient way in class but this works at least...
-    return $"{FirstName {(string.IsNullOrWhiteSpace(MiddleName) ? String.Empty : " +  {MiddleName} {LastName}";
-
+    {
+    return $"{firstName}{(string.IsNullOrWhiteSpace(middleName) ? String.Empty : " +  middleName)} {LastName}")}";
     }
 
 }
