026593b4dd891496ac870640e63673dc1929c96a
Purplepigs0129 <praythopson@ewu.edu>
2024-02-08 15:18:50 -0800

    updated storagetests

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -3,35 +3,36 @@ namespace Logger.Tests;
 
 public class StorageTests
 {
+    Storage Storage { get; set; }
     public StorageTests()
     {
-        Storage storage = new();
+        Storage = new();
     }
 
     //Student
     [Fact]
     public void Add_Student_ContainsStudent()
     {
-        Student student = new(new FullName("First", "Middle", "Last"));
-        storage.Add(student);
-        Assert.True(storage.Contains(student));
+        Student student = new Student(new FullName("Dean", "Winchester", null));
+        Storage.Add(student);
+        Assert.True(Storage.Contains(student));
     }
 
     [Fact]
     public void Remove_Student_DoesNotContainStudent()
     {
-        Student student = new(new FullName("First", "Middle", "Last"));
-        storage.Add(student);
-        storage.Remove(student);
-        Assert.False(storage.Contains(student));
+        Student student = new(new FullName("Samuel", "Winchester", "Mike"));
+        Storage.Add(student);
+        Storage.Remove(student);
+        Assert.False(Storage.Contains(student));
     }
 
     [Fact]
     public void Get_Student_ReturnsStudentsID()
     {
-        Student student = new(new FullName("First", "Middle", "Last"));
-        storage.Add(student);
-        Assert.Equal(student, storage.Get(student.Id));
+        Student student = new(new FullName("John", "Winchester", "Curt"));
+        Storage.Add(student);
+        Assert.Equal(student, Storage.Get(student.Id));
     }
 
     //Employee
@@ -39,55 +40,55 @@ public class StorageTests
     public void Add_Employee_ContainsEmployee()
     {
         
-        Employee employee = new(new FullName("First", "Middle", "Last"));
-        storage.Add(employee);
-        Assert.True(storage.Contains(employee));
+        Employee employee = new(new FullName("Mary", "Winchester", "Rose"));
+        Storage.Add(employee);
+        Assert.True(Storage.Contains(employee));
     }
 
     [Fact]
     public void Remove_Employee_DoesNotContainEmployee()
     {
         
-        Employee employee = new(new FullName("First", "Middle", "Last"));
-        storage.Add(employee);
-        storage.Remove(employee);
-        Assert.False(storage.Contains(employee));
+        Employee employee = new(new FullName("Carmen", "Berzatto", "Bear"));
+        Storage.Add(employee);
+        Storage.Remove(employee);
+        Assert.False(Storage.Contains(employee));
     }
 
     [Fact]
     public void Get_Employee_ReturnsEmployeesID()
     {
         
-        Employee employee = new(new FullName("First", "Middle", "Last"));
-        storage.Add(employee);
-        Assert.Equal(employee, storage.Get(employee.Id));
+        Employee employee = new(new FullName("Natalie", "Berzatto", "Sugar"));
+        Storage.Add(employee);
+        Assert.Equal(employee, Storage.Get(employee.Id));
     }
     //Book
     [Fact]
     public void Add_Book_ContainsBook()
     {
         
-        Book book = new("Title");
-        storage.Add(book);
-        Assert.True(storage.Contains(book));
+        Book book = new("War and Peace");
+        Storage.Add(book);
+        Assert.True(Storage.Contains(book));
     }
 
     [Fact]
     public void Remove_Book_DoesNotContainBook()
     {
         
-        Book book = new("Title");
-        storage.Add(book);
-        storage.Remove(book);
-        Assert.False(storage.Contains(book));
+        Book book = new("Moby Dick");
+        Storage.Add(book);
+        Storage.Remove(book);
+        Assert.False(Storage.Contains(book));
     }
 
     [Fact]
     public void Get_Book_ReturnsBooksID()
     {
         
-        Book book = new("Title");
-        storage.Add(book);
-        Assert.Equal(book, storage.Get(book.Id));
+        Book book = new("Farenheit 451");
+        Storage.Add(book);
+        Assert.Equal(book, Storage.Get(book.Id));
     }
 }
