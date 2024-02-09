10bbae9294a1b0574c754af7592296a4a76b17d2
Purplepigs0129 <praythopson@ewu.edu>
2024-02-08 21:53:49 -0800

    fixed from merge conflicts

--- Logger/Book.cs
+++ Logger/Book.cs
@@ -2,14 +2,11 @@ namespace Logger;
 
 public record class Book(string Title) : BaseEntity
 {
-    //TODO FIX
-    public string Title {get;} = Title ?? ArgumentNullException.ThrowIfNull(nameof(title));
     public override string Name { 
     get=>$"{Title}";}
     public string Title { get; } = string.IsNullOrEmpty(Title) ? throw new ArgumentNullException(nameof(Title)) : Title;
 
     //Implimented implicitly since Name isn't causing any collisions, and if we ever need an Author name we would
     // probably add an Author property of Entity type person which would get the Author name through that.
-    public override string Name { get => Title; }
 
 }
