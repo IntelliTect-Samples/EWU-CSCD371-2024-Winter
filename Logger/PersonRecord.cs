9dc1eb56a3a402167b644d1309ed3e41c99c8528
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-08 16:50:25 -0800

    refactored the person class

--- Logger/PersonRecord.cs
+++ Logger/PersonRecord.cs
@@ -6,7 +6,7 @@ using System.Runtime.ExceptionServices;
 
 namespace Logger;
 
-public record class PersonRecord(FullNameRecord FullName) : IEntity
+public record class PersonRecord(FullNameRecord FullName) : BaseEntity
 {
     public PersonRecord(string First, string Last, string Middle) : this(new FullNameRecord(First, Last, Middle))
     {
@@ -22,6 +22,5 @@ public record class PersonRecord(FullNameRecord FullName) : IEntity
 
     // Implicit Implementation:
     // Because it makes logical sense for the StudentRecord to have easy access to the name property.
-    public string Name { get; } = FullName.ToString();
-
+    public override string Name { get; } = FullName.ToString();
 }
