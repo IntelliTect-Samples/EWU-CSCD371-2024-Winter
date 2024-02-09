5883e963e0110b89d0101dbf3b8cd85640d6a393
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-08 22:04:02 -0800

    added LoggerFactory class and test file. Failing test for creating file logger

---
+++ Logger.Tests/LoggerFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+namespace Logger.Tests;
+
+    public class LoggerFactoryTests
+    {
+
+    [Fact]
+    public void CreateLogger_FileLoggerValidConfigruation_ReturnsFileLogger()
+    {
+        LoggerFactory<FileLogger> factory = new();
+
+        var fileLogger = factory.CreateLogger<FileLoggerConfiguration>(new FileLoggerConfiguration("Path", "Logger"));
+
+        Assert.IsType<FileLogger>(fileLogger);
+
+    }
+
+    }
+
---
+++ Logger/LoggerFactory.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Logger;
+
+public class LoggerFactory<T>
+{
+    public object CreateLogger<T1>(T1 fileLoggerConfiguration)
+    {
+        throw new NotImplementedException();
+    }
+}
