d137d684f62b85962d9ae758aa6550867fbc9034
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-08 21:23:11 -0800

    fixing potential biold errs

--- GenericsHomework/GenericsHomework.Tests/UnitTest1.cs
+++ GenericsHomework/GenericsHomework.Tests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace GenericsHomework.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace GenericsHomework.Tests;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
     }
 }
