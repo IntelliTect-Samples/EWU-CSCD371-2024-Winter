60819b8e303a971f6b37a5198da9c1eecaf6a4eb
Trevor Felchlin <tfelchlin@ewu.edu>
2024-02-08 20:13:17 -0800

    Fix Swapped Last and Middle Name for Readability

--- Logger.Tests/FullNameTests.cs
+++ Logger.Tests/FullNameTests.cs
@@ -7,26 +7,26 @@ public class FullNameTests
     [Theory]
     [InlineData("First", "Last", "Middle", "First Middle Last")]
     [InlineData("First", "Last", null, "First Last")]
-    public void FullName_ToString_HandlesNullMiddle(string first, string last, string? middle, string expects)
+    public void FullName_ToString_HandlesNullMiddle(string first, string? middle, string last, string expects)
     {
-        FullName name = new(first, last, middle);
+        FullName name = new(first, middle,last);
         Assert.Equal(expects, name.ToString());
     }
 
     [Fact]
     public void FullName_ToString_HandlesOptionalMiddle()
     {
-        FullName name = new("First", "Last");
+        FullName name = new("First",null, "Last");
         Assert.Equal("First Last", name.ToString());
     }
 
     [Theory]
     [InlineData("First", "Last", "Middle")]
     [InlineData("First", "Last", null)]
-    public void FullName_Deconstruct_Successful(string first, string last, string? middle)
+    public void FullName_Deconstruct_Successful(string first, string? middle, string last)
     {
-        FullName name = new(first, last, middle);
-        (string firstDeconst, string lastDeconst, string? middleDeconst) = name;
+        FullName name = new(first, middle, last);
+        (string firstDeconst, string? middleDeconst, string lastDeconst) = name;
         Assert.Equal(first, firstDeconst);
         Assert.Equal(last, lastDeconst);
         Assert.Equal(middle, middleDeconst);
