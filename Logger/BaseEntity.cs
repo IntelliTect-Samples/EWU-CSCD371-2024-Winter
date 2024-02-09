1b8673d59bea9630d6dd36737219c5c50bca18c4
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-08 17:00:28 -0800

    BaseEntity and BookRecord refactored

--- Logger/BaseEntity.cs
+++ Logger/BaseEntity.cs
@@ -4,6 +4,6 @@ namespace Logger;
 // We implemented the interface implicitly because the BaseEntity has a can-do relationship with IEntity
 public abstract record class BaseEntity : IEntity
 {
-    public Guid Id { get; init; }
+    public Guid Id { get; init; } = Guid.NewGuid();
     public abstract string Name { get; }
 }
--- Logger/BookRecord.cs
+++ Logger/BookRecord.cs
@@ -1,15 +1,16 @@
 ﻿
 namespace Logger;
 
-public record class BookRecord(string Name) : IEntity
+public record class BookRecord(string ClassName) : BaseEntity
 {
-    // we implemented implicitly:
-    // because each BookRecord object needs to have the id accessible in order to use the Get() function.
-    public Guid Id { get; init; } = Guid.NewGuid();
+    
     // we implemented explicitly:
     // because the Book record and the IEntity both have a duplicated naming convention for the Property Name.
-    string IEntity.Name { 
-        get {return $"{Name}";}
+    public override string Name 
+    {
+        get 
+        {
+            return $"{ClassName}" ?? throw new ArgumentNullException(nameof(ClassName));
         }
-
+    }
 }
