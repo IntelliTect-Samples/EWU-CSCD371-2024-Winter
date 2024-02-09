c90c7da3f4330c8def422fc57cf8c74e738a2430
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-08 17:24:49 -0800

    PersonRecord test refactor

--- Logger/PersonRecord.cs
+++ Logger/PersonRecord.cs
@@ -3,6 +3,9 @@ namespace Logger;
 
 public record class PersonRecord(string ClassName) : BaseEntity
 {
+    public PersonRecord(string ClassName, FullNameRecord FullName) : this(ClassName){
+        this.FullName = FullName;
+    }
 
     /// We implemented the Name property implicitly because it is an is-a relationship 
     public override string Name 
@@ -12,4 +15,6 @@ public record class PersonRecord(string ClassName) : BaseEntity
             return $"{ClassName}" ?? throw new ArgumentNullException(nameof(ClassName));
         }
     }
+
+    public FullNameRecord FullName {get; set;}
 }
