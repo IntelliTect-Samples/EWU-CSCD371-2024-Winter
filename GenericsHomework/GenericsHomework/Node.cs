d8e7049ff679b1d76a056b2770f615e838cc8f7b
Alexa <adarrington@ewu.edu>
2024-02-08 17:52:05 -0800

    Created Clear Test in NodeTests.cs

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -4,13 +4,13 @@ namespace GenericsHomework
 {
     public class Node<T>
     {
-        // Constructor for Node class
-            public Node(T item)
+        public Node(T item)
             {
                 Data = item ?? throw new NullReferenceException(nameof(item));
-                Next = this;//Next is non nullable ? might want to change to not use this
+                Next = this;
+            //Next is non nullable ? might want to change to not use this ? idk
             }
-        public Node<T> Next { get; private set; }//next setter must be private 
+        public Node<T> Next { get; private set; }
         public T Data { get; set; }
 
         public void Append(T item)
@@ -24,8 +24,10 @@ namespace GenericsHomework
             currentNode.Next = new Node<T>(item);
         }
 
-
-
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
--- GenericsHomework/GenericsTests/NodeTests.cs
+++ GenericsHomework/GenericsTests/NodeTests.cs
@@ -5,18 +5,33 @@ namespace GenericsTests
     {
         [Fact]
 
-        public void NodeConstructor_NullNode_ThrowsNulReferenceException()
+        public void NodeConstructor_NullNode_ThrowsNullReferenceException()
         {
             Assert.Throws<NullReferenceException>(() => new Node<object>(null!));
 
         }
         [Fact]
-        public void Append_NextNode_ReturnsValue()
+        public void Append_AddedNodes_SuccesfullyReturnsValue()
         {
-            Node<string> newNode = new Node<string>("Hiiii");
+            Node <string> newNode = new Node<string>("Hiiii");
             newNode.Append("You rock");
+            
             Assert.Equal("You rock", newNode.Next.Data);
            
+        }
+        [Fact]
+
+        public void Clear_ManyNodes_SuccessfullyClears()
+        {
+            Node<string> newNode = new Node<string>("Lets goo");
+            newNode.Append("New Node1");
+            newNode.Append("New Node2");
+            newNode.Clear();
+
+            Assert.Equal(newNode, newNode.Next);
+
+
+
         }
 
     }
