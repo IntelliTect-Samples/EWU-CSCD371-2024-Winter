2ad91578d27cff021700f0a4a2ae04322e5ab5c9
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-08 17:17:01 -0800

    StudentRecordTeste refactor

--- Logger.Tests/StudentRecordTests.cs
+++ Logger.Tests/StudentRecordTests.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace Logger.Tests;
@@ -8,16 +7,14 @@ public class StudentRecordTests
     [Fact]
     public void StudentRecord_GetName_Success()
     {
-        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
-        StudentRecord studentRecord = new(fullName);
-        Assert.Equal("Inigo Alex Montoya", studentRecord.Name);
+        StudentRecord studentRecord = new(nameof(StudentRecord));
+        Assert.Equal(nameof(StudentRecord), studentRecord.Name);
     }
 
     [Fact]
     public void StudentRecord_SameStudentEquals_Success()
     {
-        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
-        StudentRecord student1 = new(fullName);
+        StudentRecord student1 = new(nameof(StudentRecord));
         StudentRecord student2 = student1 with {};
         Assert.True(student1.Equals(student2));
     }
@@ -25,23 +22,8 @@ public class StudentRecordTests
     [Fact]
     public void StudentRecord_TwoStudentsNotEqual_Success()
     {
-        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
-        StudentRecord student1 = new(fullName);
-        StudentRecord student2 = new(fullName);
+        StudentRecord student1 = new(nameof(StudentRecord));
+        StudentRecord student2 = new(nameof(StudentRecord));
         Assert.False(student1.Equals(student2));
-    }
-
-    [Fact]
-    public void StudentRecord_SetFirstLastWithString_Success()
-    {
-        StudentRecord studentRecord = new("Inigo", "Montoya");
-        Assert.Equal("Inigo Montoya", studentRecord.Name);
-    }
-
-    [Fact]
-    public void StudentRecord_SetFirstLastMiddleWithString_Success()
-    {
-        StudentRecord record = new("Inigo", "Montoya", "Alex");
-        Assert.Equal("Inigo Alex Montoya", record.Name);
     }
 }
