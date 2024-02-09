de60ffedae7edeac50e200bc17206a41aedc0513
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-08 17:10:18 -0800

    EmployeeRecord refactored

--- Logger/EmployeeRecord.cs
+++ Logger/EmployeeRecord.cs
@@ -1,9 +1,4 @@
 namespace Logger;
 
 // The Employee record inherits the PersonRecord where all of the refactored code was moved.
-public record class EmployeeRecord(FullNameRecord FullName) : PersonRecord(FullName)
-{
-    public EmployeeRecord(string First, string Last, string Middle) : this(new FullNameRecord(First, Last, Middle)){}
-
-    public EmployeeRecord(string First, string Last) : this(new FullNameRecord(First, Last)){}
-}
+public record class EmployeeRecord(string ClassName) : PersonRecord(ClassName);
