514c877f9a9865b3021ebebd755bd0c927b8d79a
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-08 16:29:33 -0800

    resolve comments

--- Logger/BaseEntity.cs
+++ Logger/BaseEntity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Logger;
 
-// We implemented the interface implicitly
+// We implemented the interface implicitly because the BaseEntity has a cando relationtip with IEntity
 public abstract class BaseEntity : IEntity
 {
     public Guid Id { get; init; }
