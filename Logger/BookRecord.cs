840aa6f91172c6a82815403899a2546165a3436c
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-08 16:32:12 -0800

    resolve comments

--- Logger/BookRecord.cs
+++ Logger/BookRecord.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Logger;
 
 public record class BookRecord(string Name) : IEntity
