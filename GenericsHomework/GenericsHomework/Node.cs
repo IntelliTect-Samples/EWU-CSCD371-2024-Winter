6cf5a3d9b4267ac0bf8782bf464c29d40c6b959c
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-11 14:55:12 -0800

    created the logic for the node list

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -2,12 +2,26 @@
 
 public class Node<T>
 {
-    public Node(T data)
+    private Node<T>? _Next;
+    public Node(T data, Node<T> next)
     {
         Data = data;
+        Next = next;
     }
 
     public T Data { get; set; }
-    Node<T> Next { get;  set; }
+    Node<T> Next { 
+        get{ return _Next!;}  
+        set{ 
+            ArgumentNullException.ThrowIfNull(value);
+            
+            if(value.Equals(this)){
+                _Next = this;
+            }
+            else{
+                _Next = value;
+            }
+        } 
+    }
 }
 
