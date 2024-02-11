25a76e43fb88c6eb1233ee0af439f5837db3b015
Ethan Guerin <eguerin@ewu.edu>
2024-02-11 14:27:08 -0800

    Assignment 5 - add: Created project and added Node Class

---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericsHomework;
+
+public class Node<T>
+{
+    public T Value { get; }
+    private Node<T>? next;
+    public Node<T> Next
+    {
+        get => next ?? this;
+        private set => next = value;
+    }
+}
