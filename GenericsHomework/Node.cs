c95a37acb666c5abcc7705c4c5df69e810f01c67
Ethan Guerin <eguerin@ewu.edu>
2024-02-11 14:38:20 -0800

    Assignment 5 - add: Wrote a basic first version of Node

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -9,4 +9,57 @@ public class Node<T>
         get => next ?? this;
         private set => next = value;
     }
+
+    public Node (T value)
+    {
+        Value = value;
+        next = null;
+    }
+
+    public override string ToString()
+    {
+        return Value?.ToString() ?? string.Empty;
+    }
+
+    public void Append(T value)
+    {
+        if(Exists(value))
+        {
+            throw new ArgumentException("Duplicate Value cannot be added.", nameof(value));
+        }
+        Node<T> newNode = new Node<T>(value);
+        if(Next == this)
+        {
+            Next = newNode;
+        }
+        else
+        {
+            Node<T> cur = Next;
+            while(cur.Next != this)
+            {
+                cur = cur.Next;
+            }
+            cur.Next = newNode;
+        }
+        newNode.Next = this;
+    }
+
+    public bool Exists(T value)
+    {
+        Node<T> cur = this;
+        do
+        {
+            if(EqualityComparer<T>.Default.Equals(cur.Value, value))
+            {
+                return true;
+            }
+            cur = cur.Next;
+        } while (cur != this);
+        return false;
+    }
+
+    public void Clear()
+    {
+        Next = this;
+    }
 }
