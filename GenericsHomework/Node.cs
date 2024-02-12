6e66d319e9d7072534e90ec048b47e54f02ef19b
Alexa <adarrington@ewu.edu>
2024-02-12 12:55:01 -0800

    Changed Comment in Node.cs

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -42,15 +42,17 @@ namespace GenericsHomework;
             }
         }
 
-        /* I think it is sufficient to set Next to itself because the garbage collector 
-        will be able to get back the memory and works with reference objects.
-        After some research as well, it seems the garbage collector works and handles
-        circular references and lists like our case here. MSDN says 
-        "If a group of objects contain references to each other, but none of these object 
-        are referenced directly or indirectly from stack or shared variables, 
-        then garbage collection will automatically reclaim the memory.
-        */
-        public void Clear()
+    /* I think it is sufficient to set Next to itself because the garbage collector 
+    will be able to get back the memory and works with reference objects.
+    After some research as well, it seems the garbage collector works and handles
+    circular references and lists like our case here. 
+    The high level view of how the garbage collector works:
+    Start with locals, statics and GC pinned objects. None of these can be collected
+    Mark every object which can be reached by traversing the children of these objects Collect every object which is not marked.
+    This allows for circular references to be collected just fine. 
+    So long as none of them are reachable from an object known to be uncollectable then the circular reference is essentially irrelevant.
+    */
+    public void Clear()
         {
             Next = this;
 
