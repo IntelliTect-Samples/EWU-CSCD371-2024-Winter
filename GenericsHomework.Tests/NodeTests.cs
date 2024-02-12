4d32b2aa18716dcb3829d7bcb5801332c59c9f16
Trevor Stewart <tstewart12@ewu.edu>
2024-02-11 22:53:41 -0800

    Node, Append, and Exists created, still needs tests

--- GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework.Tests/NodeTests.cs
@@ -11,6 +11,27 @@ namespace GenericsHomework.Tests;
 
 public class NodeTests
 {
-    
+    [Fact]
+    public void Node_OneNodeLoopsOnItself_Equal()
+    {
+        Node<int> node = new(3);
+        Assert.Equal(node.Data, node.Next.Data);
+    }
+
+    [Fact]
+    public void Node_MultipleNodes_NotEqual()
+    {
+        Node<int> node = new(3);
+        node.Append(5);
+        Assert.NotEqual(node, node.Next);
+    }
+
+    [Fact]
+    public void Node_LoopsAroundToSelf_Equal()
+    {
+        Node<int> node = new(3);
+        node.Append(5);
+        Assert.Equal(node, node.Next.Next);
+    }
 }
 
--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -6,8 +6,61 @@ using System.Threading.Tasks;
 
 namespace GenericsHomework;
 
-public class Node<T>
+public class Node<T> //Circular LinkedList
 {
+    public T Data { get; set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
+    {
+        this.Data = value;
+        Next = this;//else refers back to itself if there are no other nodes in the list.
+    }
+
+
+    public override string ToString()
+    {
+        //Add a ToString() override that writes out the value's ToString() result.
+        return "";
+    }
+
+    public void Append(T value)
+    {
+        if (value == null) { throw new ArgumentNullException(nameof(value)); }//Prevent null values from being added
+
+        //Throw an error on an attempt to Append a duplicate value. (Make sure you test for this case)
+        if(Exists(value) == true) throw new Exception("Cannot have duplicate: " + value.ToString());
+
+        Node<T> cur = this;
+
+        while(cur.Next != this)
+        {
+            cur = cur.Next;
+        }
+
+        Node<T> newNode = new(value);
+        cur.Next = newNode;
+        newNode.Next = this;//Loop the newNode back to the head
+    }
+
+    public void Clear()
+    {
+
+    }
+
+    public bool Exists(T value)
+    {
+        //Search list to see if value exists
+        Node<T> cur = this;
+        while( cur.Next != this)
+        {
+            if(cur.Data!.Equals(value)) return true; //Using ! since Append checks if value is null
+            cur = cur.Next;
+        }
+
+        return false;
+    }
+
 
 }
 
