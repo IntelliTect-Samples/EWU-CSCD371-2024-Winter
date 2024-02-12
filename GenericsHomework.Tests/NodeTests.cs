766ee1d16c8c35f810b01fb07dc9b5fe931b7967
Trevor Stewart <tstewart12@ewu.edu>
2024-02-11 19:54:05 -0800

    Initial project setup

---
+++ GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GenericsHomework.Tests;
+
+//Reminder: For this assignment, always use Assert.AreEqual<T>() (the generic version)
+
+public class NodeTests
+{
+    
+}
+
