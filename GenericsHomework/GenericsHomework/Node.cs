d460f8e4d70107c5e2045f277ef81f688e1d400b
Trevor Felchlin <tfelchlin@ewu.edu>
2024-02-12 19:44:05 -0800

    Added tests and initial node code

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,8 +1,50 @@
 ﻿namespace GenericsHomework;
-public class Node
+public class Node<T>
 {
-    static void Main(string[] args)
+    public T Value { get; }
+    public Node<T> Next { get; private set; }
+    public Node(T value)
     {
-        Console.WriteLine("Hello, World!");
+        Value = value ?? throw new ArgumentException(nameof(Value));
+        Next = this; // Points back to itself by default
+    }
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+
+    public void Clear()
+    {
+        Next = this;
+    }
+    public void Append(T value)
+    {
+        if (Exists(value))
+        {
+            throw new ArgumentException("Duplicate value detected.");
+        }
+
+        Node<T> newNode = new(value)
+        {
+            Next = Next
+        };
+        Next = newNode;
+    }
+    public bool Exists(T value)
+    {
+        Node<T> current = this;
+        do
+        {
+            if (current.Value.Equals(value))
+            {
+                return true;
+            }
+            else
+            {
+                current = current.Next;
+            }
+        } while (current != this);
+        return false;
     }
 }
+
--- GenericsHomework/GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -1,11 +1,63 @@
+using GenericsHomework;
 using Xunit;
 
 namespace GenericsHomeworkTests;
 public class NodeTests
 {
     [Fact]
-    public void Test1()
+    public void Node_WithIntegerValue_HasCorrectValue()
     {
+        Node<int> node = new(5);
+        Assert.Equal(5, node.Value);
+    }
+    [Fact]
+    public void Node_ToString_ReturnsCorrectStringRepresentation()
+    {
+        Node<string> node = new("Hello");
+        Assert.Equal("Hello", node.ToString());
+    }
+    [Fact]
+    public void Append_AddsNewNodeAfterCurrentNode()
+    {
+        Node<int> firstNode = new(1);
+        firstNode.Append(2);
+        Assert.Equal(2, firstNode.Next.Value);
+    }
+    [Fact]
+    public void Clear_RemovesAllNodesExceptCurrentNode_Successful()
+    {
+        Node<int> firstNode = new(1);
+        firstNode.Append(2);
+        firstNode.Append(3);
 
+        firstNode.Clear();
+
+        Assert.Equal(firstNode, firstNode.Next);
+    }
+    [Fact]
+    public void Exists_ReturnsTrueIfValueExistsInList()
+    {
+        Node<int> firstNode = new (1);
+        firstNode.Append(2);
+        firstNode.Append(3);
+
+        Assert.True(firstNode.Exists(2));
+    }
+    [Fact]
+    public void Exists_ReturnsFalseIfValueDoesNotExistInList()
+    {
+        Node<int> firstNode = new (1);
+        firstNode.Append(2);
+        firstNode.Append(3);
+
+        Assert.False(firstNode.Exists(4));
+    }
+    [Fact]
+    public void Append_ThrowsExceptionIfDuplicateValueIsAppended()
+    {
+        Node<int> firstNode = new Node<int>(1);
+        firstNode.Append(2);
+
+        Assert.Throws<System.ArgumentException>(() => firstNode.Append(2));
     }
 }
