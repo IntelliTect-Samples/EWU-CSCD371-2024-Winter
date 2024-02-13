93d4aa7515bc99dd3bcff04a8a689ef3c39a22fc
Paul <paguilar4@ewu.edu>
2024-02-13 11:07:56 -0800

    Recreated Tests file
    tests run now

---
+++ GenericsHomework.Tests/GenericsHomework.Tests/NodeTests.cs
@@ -0,0 +1,18 @@
+namespace GenericsHomework.Tests
+{
+    public class NodeTests
+    {
+       [Fact]
+    public void SingleNodePointsToItself()
+        {
+            // Arrange
+            Node<string> newNode = new Node<string>("Hiiii");
+
+            // Act - Nothing to act on for this test
+
+            // Assert
+            Assert.NotNull(newNode.Next); // Ensure Next property is not null
+            Assert.Equal(newNode, newNode.Next); // Ensure Next property points to itself
+        }
+    }
+}
