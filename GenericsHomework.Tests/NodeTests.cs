9c2e3423f433f122c41d51a3be66199b3df0596b
Andree Ramirez <aramirez61@ewu.edu>
2024-02-15 10:03:01 -0800

    added another to string test, removed comments

--- GenericsHomework.Tests/NodeTests.cs
+++ GenericsHomework.Tests/NodeTests.cs
@@ -6,12 +6,9 @@ namespace GenericsHomework.Tests;
        [Fact]
         public void Node_SingleNode_PointsToSelf()
             {
-                // Arrange
                 Node<string> newNode = new Node<string>("hola");
 
-                // Act - Nothing to act on for this test
 
-                // Assert
                 Assert.NotNull(newNode.Next); // Ensure Next property is not null
                 Assert.Equal(newNode, newNode.Next); // Ensure Next property points to itself
             }
@@ -38,13 +35,10 @@ namespace GenericsHomework.Tests;
         [Fact]
         public void Exists_ValueExists_ReturnsTrue()
         {
-            // Arrange
             Node<string> node = new Node<string>("Hello");
 
-            // Act
             bool exists = node.Exists("Hello");
 
-            // Assert
             Assert.True(exists);
         }
 
@@ -52,27 +46,32 @@ namespace GenericsHomework.Tests;
         [Fact]
         public void Exists_ValueDoesNotExist_ReturnsFalse()
         {
-            // Arrange
             Node<string> node = new Node<string>("Hello");
 
-            // Act
             bool exists = node.Exists("World");
 
-            // Assert
             Assert.False(exists);
         }
 
         [Fact]
         public void ToString_StringMatch_ReturnsCorrectStringRepresentation()
         {
-            // Arrange
             Node<int> node = new Node<int>(42);
 
-            // Act
             string result = node.ToString();
 
-            // Assert
             Assert.Equal("42", result);
+        }
+
+        [Fact]
+        public void ToString_HeadsNext_ReturnsCorrectStringRepresentation()
+        {
+            Node<int> node = new Node<int>(42);
+            node.Append(16);
+
+            string result = node.Next.ToString();
+
+            Assert.Equal("16", result);
         }
 
         [Fact]
