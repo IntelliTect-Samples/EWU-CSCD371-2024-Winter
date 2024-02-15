691749c3d47c7fc3539efc4f2a9cdece1eeb98aa
Alexa <adarrington@ewu.edu>
2024-02-15 14:10:45 -0800

    Added Circle Constructor Test and refactored to Pass in Circle.cs

---
+++ GenericsHomework/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework
+{
+    public class Circle<T>
+    {
+        public string Name { get; }
+        public Node<T>? Elements { get; private set; }
+
+        public Circle(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+         }       
+        }
+    }
