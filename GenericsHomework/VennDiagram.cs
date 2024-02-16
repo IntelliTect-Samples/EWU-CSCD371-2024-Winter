48bffa30d56ab221897d28f5c9a43786bfaba7fb
Alexa <adarrington@ewu.edu>
2024-02-15 17:27:05 -0800

    Created Constructor Test in VennDiagram Tests and refactored VennDiagram.cs to Pass

---
+++ GenericsHomework/VennDiagram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework
+{
+    public class VennDiagram<T>
+    {
+        public List<Circle<T>> Circles { get; private set; }
+        public T Data { get; }
+        public VennDiagram(IEnumerable<Circle<T>> circles,T data)
+        {
+            Circles= new List<Circle<T>>(circles);
+            Data = data;
+        }
+
+
+
+    }
+}
---
+++ GenericsTests/VennDiagramTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GenericsHomework.Tests
+{
+    public class VennDiagramTests
+    {
+        [Fact]
+        public void Constructor_ValidItem_SetsDataSuccessfully()
+        { 
+
+            Circle<string> circle1 = new Circle<string>("Circle1");
+            Circle<string> circle2 = new Circle<string>("Circle2");
+
+            // Create a list of circles
+            var circles = new List<Circle<string>> { circle1, circle2 };
+
+            // Act
+            VennDiagram<string> venn = new VennDiagram<string>(circles, "TestData");
+
+            // Assert
+            Assert.Equal("TestData", venn.Data);
+
+            // Check if circles are set correctly
+            Assert.Equal(2, venn.Circles.Count); // Check the count of circles
+            Assert.Contains(circle1, venn.Circles); // Check if circle1 is present
+            Assert.Contains(circle2, venn.Circles); // Check if circle2 is present
+        }
+
+    }
+
+    
+}
