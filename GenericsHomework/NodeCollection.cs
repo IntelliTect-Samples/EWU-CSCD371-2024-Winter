c34a1fbf42caf1957b2b058493e375b6b9271521
tfelchlin13 <tfelchlin@ewu.edu>
2024-02-15 19:41:31 -0800

    Renamed to nodecollection

---
+++ GenericsHomework/NodeCollection.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+
+namespace GenericsHomework;
+public class NodeCollection<T> : IEnumerator<T>, ICollection<T> where T : notnull
+{
+    public T Value { get; }
+    public NodeCollection<T> Next { get; private set; }
+
+    public int Count { get
+        {
+            int n = 1;
+            Reset();
+            while(MoveNext())
+            {
+                n++;
+            }
+            Reset();
+            return n;
+        } 
+    }
+
+    public bool IsReadOnly => false;
+
+    private NodeCollection<T> _current;
+    private bool _atBeginning = true;
+    public T Current => _current.Value;
+
+    object IEnumerator.Current => Current;
+
+    public NodeCollection(T value)
+    {
+        Value = value ?? throw new ArgumentException(nameof(Value));
+        Next = this; // Points back to itself by default
+        _current = this;
+    }
+    public override string ToString()
+    {
+        return Value.ToString() ?? "";
+    }
+    //we should not have to worry about garbage collection because we dont see any of the disconnected nodes and they will get cleaned up by the compiler for us. 
+    public void Clear()
+    {
+        Next = this;
+    }
+
+    public void Append(T value)
+    {
+        if (Exists(value))
+        {
+            throw new ArgumentException(null, nameof(value));
+        }
+
+        NodeCollection<T> newNode = new(value)
+        {
+            Next = Next
+        };
+        Next = newNode;
+    }
+    public bool Exists(T value)
+    {
+        NodeCollection<T> current = this;
+        for(int i = 0; i < Count; i++)
+        {
+            if(current.Value.Equals(value))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
+    }
+
+    public void Add(T item) => Append(item);
+
+    public bool Contains(T item) => Exists(item);
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(arrayIndex, nameof(arrayIndex));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(arrayIndex + Count, array.Length, nameof(arrayIndex));
+        NodeCollection<T> current = this;
+        for(int i = arrayIndex; i < Count; i++)
+        {
+            array[i] = current.Value;
+            current = current.Next;
+        }
+    }
+
+    public bool Remove(T item)
+    {
+        NodeCollection<T> current = this;
+        do
+        {
+            if(current.Next.Value.Equals(item))
+            {
+                current.Next = current.Next.Next;
+                return true;
+            }
+            current = current.Next;
+        } while(current.Next != this);
+        return false;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return this;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this;
+    }
+
+    public bool MoveNext()
+    {
+        if(_current.Next.Value.Equals(Value) && !_atBeginning)
+        {
+            return false;
+        }
+        _current = _current.Next;
+        _atBeginning = false;
+        return true;
+    }
+
+    public void Reset()
+    {
+        _current = this;
+        _atBeginning = true;
+    }
+
+    public void Dispose()
+    {
+        Clear();
+        GC.SuppressFinalize(this);
+    }
+}
+
--- GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomeworkTests/NodeTests.cs
@@ -7,21 +7,21 @@ public class NodeTests
     [Fact]
     public void Node_WithIntegerValue_HasCorrectValue()
     {
-        Node<int> node = new(5);
+        NodeCollection<int> node = new(5);
         Assert.Equal(5, node.Value);
     }
 
     [Fact]
     public void Node_ToString_ReturnsCorrectStringRepresentation()
     {
-        Node<string> node = new("Hello");
+        NodeCollection<string> node = new("Hello");
         Assert.Equal("Hello", node.ToString());
     }
 
     [Fact]
     public void Append_AddsNewNodeAfterCurrentNode()
     {
-        Node<int> firstNode = new(1);
+        NodeCollection<int> firstNode = new(1);
         firstNode.Append(2);
         Assert.Equal(2, firstNode.Next.Value);
     }
@@ -29,7 +29,7 @@ public class NodeTests
     [Fact]
     public void Clear_RemovesAllNodesExceptCurrentNode_Successful()
     {
-        Node<int> firstNode = new(1);
+        NodeCollection<int> firstNode = new(1);
         firstNode.Append(2);
         firstNode.Append(3);
 
@@ -40,7 +40,7 @@ public class NodeTests
     [Fact]
     public void Exists_ReturnsTrueIfValueExistsInList()
     {
-        Node<int> firstNode = new(1);
+        NodeCollection<int> firstNode = new(1);
         firstNode.Append(2);
         firstNode.Append(3);
 
@@ -50,7 +50,7 @@ public class NodeTests
     [Fact]
     public void Exists_ReturnsFalseIfValueDoesNotExistInList()
     {
-        Node<int> firstNode = new(1);
+        NodeCollection<int> firstNode = new(1);
         firstNode.Append(2);
         firstNode.Append(3);
         Assert.False(firstNode.Exists(4));
@@ -59,7 +59,7 @@ public class NodeTests
     [Fact]
     public void Append_ThrowsExceptionIfDuplicateValueIsAppended()
     {
-        Node<int> firstNode = new(1);
+        NodeCollection<int> firstNode = new(1);
         firstNode.Append(2);
         Assert.Throws<ArgumentException>(() => firstNode.Append(2));
         Assert.Throws<ArgumentException>(() => firstNode.Add(2));
@@ -68,7 +68,7 @@ public class NodeTests
     [Fact]
     public void Contains_ReturnsTrueIfContains()
     {
-        Node<int> firstNode = new(1);
+        NodeCollection<int> firstNode = new(1);
         Assert.DoesNotContain(2, firstNode);
         Assert.Contains(1, firstNode);
         firstNode.Append(2);
@@ -78,7 +78,7 @@ public class NodeTests
     [Fact]
     public void Count_ReturnsCorrectValue()
     {
-        Node<int> firstNode = new(1);
+        NodeCollection<int> firstNode = new(1);
         Assert.Single(firstNode);
         firstNode.Append(2);
         firstNode.Append(3);
@@ -88,7 +88,7 @@ public class NodeTests
     [Fact]
     public void CopyTo_ThrowsArgumentOutOfRangeException()
     {
-        Node<int> firstNode = new(4);
+        NodeCollection<int> firstNode = new(4);
         firstNode.Append(5);
         int[] ints = { 1, 2, 3 };
         Assert.Throws<ArgumentOutOfRangeException>(() => firstNode.CopyTo(ints, -1));
@@ -98,7 +98,7 @@ public class NodeTests
     [Fact]
     public void CopyTo_CopiesValues()
     {
-        Node<int> firstNode = new(5);
+        NodeCollection<int> firstNode = new(5);
         firstNode.Append(6);
         firstNode.Append(7);
         int[] ints = { 0, 1, 2, 3, 4 };
