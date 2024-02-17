5da31d534da9f05ef12d55f0163b391c59377e64
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-17 11:21:28 -0800

    refactor again

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -3,6 +3,14 @@ namespace Calculate;
 
 public class Calculator
 {
+
+    public static double Add(int value1, int value2)
+    {
+        ArgumentNullException.ThrowIfNull(value1);
+        ArgumentNullException.ThrowIfNull(value2);
+
+        return value1 + value2;
+    }
     public static int Multiple(int multiplicand, int multiplier)
     {
         return multiplicand * multiplier;
