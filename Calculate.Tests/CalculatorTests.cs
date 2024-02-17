59810781595f9cfb8cfa796311ccae74042d0c22
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-17 11:07:07 -0800

    Add test and method

--- Calculate.Tests/CalculatorTests.cs
+++ Calculate.Tests/CalculatorTests.cs
@@ -2,5 +2,10 @@
 
 public class CalculatorTests
 {
-
+    [Fact]
+    public void Add_NullParameters_ThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(
+            () => Calculator.Add(null!, null!));
+    }
 }
--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -1,6 +1,10 @@
-﻿namespace Calculate;
+﻿
+namespace Calculate;
 
 public class Calculator
 {
-
+    public static void Add(object value1, object value2)
+    {
+        throw new NotImplementedException();
+    }
 }
