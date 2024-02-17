5db8b42cd2baf1c1d6c0f6845e2ac9df129f1f28
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-17 11:07:54 -0800

    created the multiply and div methods

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -2,5 +2,13 @@
 
 public class Calculator
 {
+    public static int Multilple(int multiplicand, int multiplier)
+    {
+        return multiplicand * multiplier;
+    }
 
+    public static double Divide(int dividend, int divisor)
+    {
+        return dividend / (double) divisor;
+    }
 }
