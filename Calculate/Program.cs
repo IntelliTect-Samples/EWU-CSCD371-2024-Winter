917c0143ddd4babe512888fa7dcf276b7d740174
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-18 17:09:50 -0800

    disabled nullable around the properties

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -5,8 +5,11 @@ namespace Calculate;
 public class Program
 {
 
-    public Action<string>? WriteLine { get; init; }
-    public Func<string>? ReadLine { get; init; }
+    //these two propperties will never be null because since it is an init property it ensures that they are set at either object initialization or in a construcator
+    #nullable disable
+    public Action<string> WriteLine { get; init; }
+    public Func<string> ReadLine { get; init; }
+    #nullable enable
 
     public static void Main(string[] args )
     {
