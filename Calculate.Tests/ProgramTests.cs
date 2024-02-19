00c4ac54e315544c85b5682dac9583ba35d363b8
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-18 17:35:36 -0800

    A lot of stuff

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -7,7 +7,45 @@ public class ProgramTests
     [Fact]
     public void WriteLine_OutputMatch_True()
     {
-        
+        string input = "Here is my string and nothing else";
+        MockClass mockClass = new();
+        Program program = new()
+        {
+            WriteLine = mockClass.WriteLine,
+        };
 
+        program.WriteLine(input);
+
+        Assert.Equal(input, mockClass.Output);
+
+    }
+
+    [Fact]
+    public void ReadLine_OutputMatch_Ture()
+    {
+        string input = "Input? Again?";
+        MockClass mockClass = new();
+        Program program = new()
+        {
+            WriteLine = mockClass.WriteLine,
+            ReadLine = mockClass.ReadLine,
+        };
+        mockClass.Output = input;
+        Assert.Equal(input, program.ReadLine());
+
+    }
+}
+
+public class MockClass
+{
+    public string Output { get; set; }
+    public void WriteLine(string input)
+    {
+        Output = input;
+    }
+
+    public string ReadLine()
+    {
+        return Output;
     }
 }
--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -6,14 +6,19 @@ public class Program
 {
 
     //these two propperties will never be null because since it is an init property it ensures that they are set at either object initialization or in a construcator
-    #nullable disable
-    public Action<string> WriteLine { get; init; }
-    public Func<string> ReadLine { get; init; }
-    #nullable enable
+#nullable disable
+    public Action<string> WriteLine { get; init; } = Console.WriteLine;
+    public Func<string> ReadLine { get; init; } = Console.ReadLine;
+#nullable enable
+
+    public Program() { }
 
     public static void Main(string[] args )
     {
-
+        Program program = new();
+        program.WriteLine("Please enter something: ");
+        string input = program.ReadLine();
+        program.WriteLine($"You entered: {input}");
     }
 
 }
