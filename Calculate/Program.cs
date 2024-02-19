b9a712ebf448a2e274c51f2c8407e4a393627028
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-18 17:06:21 -0800

    Delegates

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Calculate;
+﻿using System.Net.Http.Headers;
+
+namespace Calculate;
 
 public class Program
 {
+
+    public Action<string>? WriteLine { get; init; }
+    public Func<string>? ReadLine { get; init; }
+
     public static void Main(string[] args )
     {
 
