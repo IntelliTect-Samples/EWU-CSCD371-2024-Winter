6057fac82286a4a86ae95b1b6e5631497023c76b
Trevor Felchlin <tfelchlin@ewu.edu>
2024-02-19 18:30:37 -0800

    Added add subtract multiply and divide

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculate
+namespace Calculate;
+public class Calculator
 {
-    internal class Calculator
+    public IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations { get; } = new Dictionary<char, Func<int, int, int>>
     {
-    }
+        ['+'] = Add,
+        ['-'] = Subtract,
+        ['*'] = Multiply,
+        ['/'] = Divide
+    };
+
+    public static int Add(int a, int b) => a + b;
+    public static int Subtract(int a, int b) => a - b;
+    public static int Multiply(int a, int b) => a * b;
+    public static int Divide(int a, int b) => b != 0 ? a / b : throw new ArgumentException("Can't divide by 0", nameof(b));
 }
+
--- Calculate/CalculateTests/CalculatorTests.cs
+++ Calculate/CalculateTests/CalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,50 @@ using Xunit;
 namespace CalculateTests;
 public class CalculatorTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(3, 4, 7)]
+    [InlineData(42, 2, 44)]
+    [InlineData(6, 7, 13)]
+    [InlineData(20, 5, 25)]
+    public void TestAdd(int a, int b, int expected)
     {
+        int result = Calculator.Add(a, b);
 
+        Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData(3, 4, -1)]
+    [InlineData(42, 2, 40)]
+    [InlineData(6, 7, -1)]
+    [InlineData(20, 5, 15)]
+    public void TestSubtract(int a, int b, int expected)
+    {
+        int result = Calculator.Subtract(a, b);
+
+        Assert.Equal(expected, result);
+    }
+    [Theory]
+    [InlineData(3, 4, 12)]
+    [InlineData(42, 2, 84)]
+    [InlineData(6, 7, 42)]
+    [InlineData(20, 5, 100)]
+    public void TestMultiply(int a, int b, int expected)
+    {
+        int result = Calculator.Multiply(a, b);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(6, 3, 2)]
+    [InlineData(40, 2, 20)]
+    [InlineData(49, 7, 7)]
+    [InlineData(20, 5, 4)]
+    public void TestDivide(int a, int b, int expected)
+    {
+        int result = Calculator.Divide(a, b);
+
+        Assert.Equal(expected, result);
     }
 }
 
