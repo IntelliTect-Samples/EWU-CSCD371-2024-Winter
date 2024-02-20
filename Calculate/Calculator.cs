16c6059039de0adf64276260ef86b87a24e26fd8
Paul <paguilar4@ewu.edu>
2024-02-19 22:49:57 -0800

    Added MathematicalOperation

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -14,5 +14,14 @@ namespace Calculate;
     public static int Multiply(int x, int y) => x * y;
     public static int Divide(int x, int y) => x / y;
 
+    public IReadOnlyDictionary<char, Func<double, double, double>> MathematicalOperations { get; } =
+    new Dictionary<char, Func<double, double, double>>
+    {
+            { '+', (a, b) => a + b },
+            { '-', (a, b) => a - b }, 
+            { '*', (a, b) => a * b }, 
+            { '/', (a, b) => a / b }  
+    };
+
 }
 
