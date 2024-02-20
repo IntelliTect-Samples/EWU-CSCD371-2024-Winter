744411b92ca27b42bf407c112b4653fe1a576f25
Trevor Stewart <tstewart12@ewu.edu>
2024-02-20 14:08:38 -0800

    Added tests for WriteLine and ReadLine

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -4,8 +4,30 @@ namespace Calculate.Tests;
 public class ProgramTests
 {
     [Fact]
-    public void Test1()
+    public void WriteLine_ValidInput_Success()
     {
+        string testString = "test string";
 
+        using StringWriter writer = new();
+        Console.SetOut(writer);
+
+        Program program = new();
+        program.WriteLine(testString);
+
+        Assert.Equal(testString + "\r\n", writer.ToString());
+    }
+
+    [Fact]
+    public void ReadLine_ValidInput_Success()
+    {
+        string testString = "test string";
+
+        using StringReader reader = new(testString);
+        Console.SetIn(reader);
+
+        Program program = new();
+        string result = program.ReadLine();
+
+        Assert.Equal(testString, result);
     }
 }
