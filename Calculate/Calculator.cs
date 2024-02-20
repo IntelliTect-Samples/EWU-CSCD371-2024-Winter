98815e8260604a3d5345603dd4056a4b42e63774
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-19 23:28:06 -0800

    added in MathematicalOperations readonly property

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -1,11 +1,14 @@
-﻿
-
-
-
-namespace Calculate;
+﻿namespace Calculate;
 
 public class Calculator
 {
+
+    public IReadOnlyDictionary<char, int> MathematicalOperations { get; } = new Dictionary<char, int>()
+        { {'+',0 },
+        {'-',1 },
+        {'-',2 },
+        {'-',3 }};
+    
     public static int Add(int num1, int num2)
     {
         return num1 + num2;
@@ -23,9 +26,6 @@ public class Calculator
     {
         return num1 / num2;
     }
-
-
-
 
 }
 
