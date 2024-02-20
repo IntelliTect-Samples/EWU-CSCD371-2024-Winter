726d568b02c7cfe0d9c953721c6fbeded77d64ca
Purplepigs0129 <praythopson@ewu.edu>
2024-02-19 18:02:20 -0800

    updated program

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,16 +1,26 @@
 ﻿namespace Calculate;
 public class Program
 {
-    public string WriteLine{ get; init;}
-    public string ReadLine { get; init;}
-
-    public Program()
-    {
-
-    }
+#nullable disable
+    public Action<string> WriteLine { get; init; } = Console.WriteLine;
+    public Func<string> ReadLine { get; init; } = Console.ReadLine;
+#nullable enable
+    public Program(){ }
 
     public static void Main(string[] args)
     {
+        Program program = new();
         Calculator calculator = new();
+        string input;
+        double? answer;
+
+        do
+        {
+            program.WriteLine("Enter Simple Equation: ");
+            input = program.ReadLine();
+        }
+        while (!calculator.TryCalculate(input, out answer));
+
+        program.WriteLine($"Answer: {answer}");
     }
 }
