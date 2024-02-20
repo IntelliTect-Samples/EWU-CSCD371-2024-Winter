9bf459b0b1dae90a70c2718a2d514e12df0c231a
Andree Ramirez <aramirez61@ewu.edu>
2024-02-19 17:29:29 -0800

    changed calculator to public, added tests for it

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculate;
-    internal class Calculator
+    public class Calculator
     {
 
 
--- CalculateTests.Test/CalculatorTests.cs
+++ CalculateTests.Test/CalculatorTests.cs
@@ -1,14 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
+using Calculate;
 
-namespace CalculateTests.Test;
-
-//XUnit tests - Assert.Equal are generic by default
-
-internal class CalculatorTests
+namespace CalculateTests.Test
+{
+    public class CalculatorTests
     {
-    }
+        [Fact]
+        public void TestAdd()
+        {
+            // Arrange
+            int x = 5;
+            int y = 3;
 
+            // Act
+            int result = Calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void TestSubtract()
+        {
+            // Arrange
+            int x = 5;
+            int y = 3;
+
+            // Act
+            int result = Calculator.Subtract(x, y);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void TestMultiply()
+        {
+            // Arrange
+            int x = 5;
+            int y = 3;
+
+            // Act
+            int result = Calculator.Multiply(x, y);
+
+            // Assert
+            Assert.Equal(15, result);
+        }
+
+        [Fact]
+        public void TestDivide()
+        {
+            // Arrange
+            int x = 6;
+            int y = 3;
+
+            // Act
+            int result = Calculator.Divide(x, y);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+    }
+}
