a9ff994adf016f4d23e9d97ac14a2025571207c1
Paul <paguilar4@ewu.edu>
2024-02-20 20:29:25 -0800

    added comments

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -3,10 +3,11 @@
 public class Program
 {
 
+    //Set the default behavior for the WriteLine and ReadLine properties to invoke System.Console versions of the methods
     public Action<string> WriteLine { get; init; } = System.Console.WriteLine;
     public Func<string?> ReadLine { get; init; } = System.Console.ReadLine;
 
-
+    //add an empty default constructor
     public Program() { }
 
     public static void Main(string[] args)
@@ -20,6 +21,8 @@ public class Program
         string? userInput;
 
         program.WriteLine("Welcome to the mighty Calcutron, please enter an expression using either +, -, *, / operators:");
+
+        //invoke it based on user input from the console
 
         userInput = program.ReadLine();
             ArgumentException.ThrowIfNullOrEmpty(userInput);
