240e7f2df73ea549056cdebab87b683432deabd7
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-20 20:35:40 -0800

    improved Program tests by checking multiple reads and writes for default and init Program properites

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -5,18 +5,27 @@ namespace Calculate.Tests;
 public class ProgramTests : IDisposable
 {
 
+    // Will never be null as it is set in constructor
     #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public static string TempPath { get; set; }
+    public static Program DefaultProgram { get; set; }
+    public static Program InitProgram { get; set; }
     #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-       
+
     public ProgramTests()
     {
         TempPath = Path.GetTempFileName();
+        DefaultProgram = new Program();
+        InitProgram = new Program()
+        {
+            ReadLine = FileReadLine,
+            WriteLine = FileWriteLine
+        };    
     }
 
     static void FileWriteLine(string value)
     {
-        using StreamWriter streamWriter = new(TempPath);
+        using StreamWriter streamWriter = new(TempPath,true);
         streamWriter.WriteLine(value);
         streamWriter.Close();
     }
@@ -27,57 +36,52 @@ public class ProgramTests : IDisposable
 
     }
 
-
     [Fact]
     public void DefaultConstructor_WriteLineProperty_ExpectedBehaviour()
     {
-        Program program = new();
         using StringWriter mockConsole = new();
 
         Console.SetOut(mockConsole);
-        program.WriteLine("Hello");
+        DefaultProgram.WriteLine("Hello");
+        DefaultProgram.WriteLine("World");
 
         Assert.Contains("Hello", mockConsole.ToString());
+        Assert.Contains("World", mockConsole.ToString());
 
     }
+
     [Fact]
     public void DefaultConstructor_ReadLineProperty_ExpectedBehaviour()
     {
-        Program program = new();
-            
-        Console.SetIn(new StringReader("Hello"));
+        Console.SetIn(new StringReader(string.Join(Environment.NewLine, ["Hello", "World"])));
 
-        Assert.Equal("Hello", program.ReadLine());
+        Assert.Equal("Hello", DefaultProgram.ReadLine());
+        Assert.Equal("World", DefaultProgram.ReadLine());
 
     }
 
     [Fact]
     public void Constructor_InitFileWriteLine_ExpectedBehavior()
     {
-        Program program = new()
-        {
-            WriteLine = FileWriteLine
-        };
-
-        program.WriteLine("Hello World");
+        InitProgram.WriteLine("Testing File Writing");
+        InitProgram.WriteLine("Testing File Writing 2");
 
         using StreamReader streamReader = new(TempPath);
-        Assert.Equal("Hello World",streamReader.ReadLine());
+        Assert.Equal("Testing File Writing", streamReader.ReadLine());
+        Assert.Equal("Testing File Writing 2", streamReader.ReadLine());
 
     }
     [Fact]
     public void Constructor_InitFileReadLine_ExpectedBehavior()
     {
-        Program program = new()
-        {
-            ReadLine = FileReadLine
-        };
-
         using StreamWriter streamWriter = new(TempPath);
-        streamWriter.WriteLine("Hello World");
+        streamWriter.WriteLine("Testing File Reading");
+        streamWriter.WriteLine("Testing File Reading 2");
+
         streamWriter.Close();
 
-        Assert.Equal("Hello World", program.ReadLine());
+        Assert.Equal("Testing File Reading", InitProgram.ReadLine());
+        Assert.Equal("Testing File Reading", InitProgram.ReadLine());
 
     }
 
