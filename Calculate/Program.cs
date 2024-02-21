435c92e500ade963b414b28d37298dc3915a7f6f
Paul <paguilar4@ewu.edu>
2024-02-20 20:21:00 -0800

    added main

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -11,9 +11,31 @@ public class Program
 
     public static void Main(string[] args)
         {
-            // Your application logic goes here
+        //Implement the Program class to instantiate the calculator
+        Program program = new();
+         
+        Calculator calcutron = new();
+
+
+        string? userInput;
+
+        program.WriteLine("Welcome to the mighty Calcutron, please enter an expression using either +, -, *, / operators:");
+
+        userInput = program.ReadLine();
+            ArgumentException.ThrowIfNullOrEmpty(userInput);
+
+        while (!calcutron.TryCalculate(userInput, out double answer))
+        {
+            program.WriteLine($"{answer}");
         }
 
-}
+
+
+
+
+
+    }//end of main
+
+}//end of class
 
 
