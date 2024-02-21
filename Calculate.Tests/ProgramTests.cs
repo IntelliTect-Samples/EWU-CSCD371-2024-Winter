ebf2384091d3d15eec290cdc37ca31ca1741c52d
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-20 19:48:05 -0800

    finished adding in additional tests for Program Writeline and ReadLine

--- Calculate.Tests/ProgramTests.cs
+++ Calculate.Tests/ProgramTests.cs
@@ -1,70 +1,85 @@
 using Xunit;
-using System.IO;
-using System.ComponentModel;
 
 namespace Calculate.Tests;
 
-    public class ProgramTests : IDisposable
-    {
+public class ProgramTests : IDisposable
+{
 
-        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public static string TempPath { get; set; }
-        #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public static string TempPath { get; set; }
+    #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
        
-        public ProgramTests()
-        {
-            TempPath = Path.GetTempFileName();
-        }
+    public ProgramTests()
+    {
+        TempPath = Path.GetTempFileName();
+    }
 
-        static void WriteLinetoStream(string value)
-        {
-            using StreamWriter streamReader = new(TempPath);
-            streamReader.WriteLine(value);
-        }
-        static string? ReadLineFromFile()
-        {
-            using StreamReader streamReader = new(TempPath);
-            return streamReader.ReadLine();
-        }
+    static void FileWriteLine(string value)
+    {
+        using StreamWriter streamWriter = new(TempPath);
+        streamWriter.WriteLine(value);
+        streamWriter.Close();
+    }
+    static string? FileReadLine()
+    {
+        using StreamReader streamReader = new(TempPath);
+        return streamReader.ReadLine();
+
+    }
 
 
-        [Fact]
-        public void DefaultConstructor_WriteLineProperty_ExpectedBehaviour()
-        {
-            Program program = new();
-            using StringWriter mockConsole = new();
+    [Fact]
+    public void DefaultConstructor_WriteLineProperty_ExpectedBehaviour()
+    {
+        Program program = new();
+        using StringWriter mockConsole = new();
 
-            Console.SetOut(mockConsole);
-            program.WriteLine("Hello");
+        Console.SetOut(mockConsole);
+        program.WriteLine("Hello");
 
-            Assert.Contains("Hello", mockConsole.ToString());
+        Assert.Contains("Hello", mockConsole.ToString());
 
-        }
-        [Fact]
-        public void DefaultConstructor_ReadLineProperty_ExpectedBehaviour()
-        {
-            Program program = new();
+    }
+    [Fact]
+    public void DefaultConstructor_ReadLineProperty_ExpectedBehaviour()
+    {
+        Program program = new();
             
-            Console.SetIn(new StringReader("Hello"));
+        Console.SetIn(new StringReader("Hello"));
 
-            Assert.Equal("Hello", program.ReadLine());
+        Assert.Equal("Hello", program.ReadLine());
 
-        }
+    }
 
-        [Fact]
-        public void Constructor_InitFileWriteLine_ExpectedBehavior()
+    [Fact]
+    public void Constructor_InitFileWriteLine_ExpectedBehavior()
+    {
+        Program program = new()
         {
-            Program program = new()
-            {
-                WriteLine = WriteLinetoStream
-            };
+            WriteLine = FileWriteLine
+        };
 
-            program.WriteLine("Hello World");
+        program.WriteLine("Hello World");
 
-            using StreamReader streamReader = new(TempPath);
-            Assert.Equal("Hello World",streamReader.ReadLine());
+        using StreamReader streamReader = new(TempPath);
+        Assert.Equal("Hello World",streamReader.ReadLine());
 
-        }
+    }
+    [Fact]
+    public void Constructor_InitFileReadLine_ExpectedBehavior()
+    {
+        Program program = new()
+        {
+            ReadLine = FileReadLine
+        };
+
+        using StreamWriter streamWriter = new(TempPath);
+        streamWriter.WriteLine("Hello World");
+        streamWriter.Close();
+
+        Assert.Equal("Hello World", program.ReadLine());
+
+    }
 
     public void Dispose()
     {
