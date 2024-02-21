38fb74557c6c253f0d50660dca23034dd39a8d99
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-20 21:04:55 -0800

    A lot of refactoring

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -1,6 +1,3 @@
-
-using System.Security.Cryptography;
-
 namespace Calculate;
 
 public class Calculator
--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,11 +1,9 @@
-﻿using System.Net.Http.Headers;
-
-namespace Calculate;
+﻿namespace Calculate;
 
 public class Program
 {
 
-    //these two propperties will never be null because since it is an init property it ensures that they are set at either object initialization or in a construcator
+    //these two properties will never be null because since it is an init property it ensures that they are set at either object initialization or in a constructor
 #nullable disable
     public Action<string> WriteLine { get; init; } = Console.WriteLine;
     public Func<string> ReadLine { get; init; } = Console.ReadLine;
