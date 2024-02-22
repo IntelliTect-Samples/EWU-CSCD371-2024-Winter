0e90287f16a4ceaa95bd8e346c2772f3dac5a17f
Paul <paguilar4@ewu.edu>
2024-02-22 10:19:39 -0800

    Updated Calculator
    Mathematical Ops. now uses methods
    Added check for dividing by zero

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -12,15 +12,20 @@ namespace Calculate;
     public static double Add(double x, double y) => x + y;
     public static double Subtract(double x, double y) => x - y;
     public static double Multiply(double x, double y) => x * y;
-    public static double Divide(double x, double y) => x / y;
+    public static double Divide(double x, double y)
+    {
+        if (y == 0)
+            throw new ArgumentException("Can't divide by zero");
+     return x / y;
+    }
 
     public IReadOnlyDictionary<char, Func<double, double, double>> MathematicalOperations { get; } =
     new Dictionary<char, Func<double, double, double>>
     {
-            { '+', (a, b) => a + b },
-            { '-', (a, b) => a - b }, 
-            { '*', (a, b) => a * b }, 
-            { '/', (a, b) => a / b }  
+            { '+', Add },
+            { '-', Subtract }, 
+            { '*', Multiply },
+            { '/', Divide }  
     };
 
     public bool TryCalculate(string expression, out double result)
