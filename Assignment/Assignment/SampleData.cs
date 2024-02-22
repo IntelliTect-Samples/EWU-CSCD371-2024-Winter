74409db13c864bf3690de99618f8c9021606349c
Ethan Guerin <eguerin@ewu.edu>
2024-02-22 14:32:12 -0800

    Assignment7 - add: Added first go at Csv methods 1-3
    Also changed People.csv copy prop, will return in few hours to add tests!

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
+        private const string CsvFilePath = "People.csv";
+
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
-
-        // 2.
+        public IEnumerable<string> CsvRows 
+            => File.ReadLines(CsvFilePath).Skip(1);// This should return the entire list, Line by Line, as a string
+            
+         // 2.
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
-
+            => CsvRows
+                .Select(row => row.Split(',')[6])
+                .Distinct()
+                .OrderBy(state => state);//This will use CsvRows, calling in one large string,
+                                         //splitting it at the 6th comma, and ordering by state
+        
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+            => string.Join(",", GetUniqueSortedListOfStatesGivenCsvRows());//This should combine them,
+                                                                           //though selecting only states I am unsure how to do 
 
         // 4.
         public IEnumerable<IPerson> People => throw new NotImplementedException();
