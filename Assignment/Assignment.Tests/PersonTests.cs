2d2bd30df0ada1a35f7101e89acb0b180833036c
Joshua Merrell <jmerrell2@ewu.edu>
2024-02-24 22:23:55 -0800

    Add Person FromCsvRow and deconstructor

---
+++ Assignment/Assignment.Tests/PersonTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment.Tests;
+
+[TestClass]
+public class PersonTests
+{
+    [TestMethod]
+    public void FromCsvRow_ThrowsArgumentExceptionOnNullOrEmpty()
+    {
+        Assert.ThrowsException<ArgumentException>(() => Person.FromCsvRow(""));
+        Assert.ThrowsException<ArgumentNullException>(() => Person.FromCsvRow(null!));
+    }
+
+    [TestMethod]
+    public void FromCsvRow_ThrowsExceptionIfNotEnoughColumns()
+    {
+        string badRow = "one,two,three,four,five,six,seven,eight,nine";
+        string goodRow = "one,two,three,four,five,six,seven,eight";
+        Assert.ThrowsException<Exception>(() => Person.FromCsvRow(badRow));
+        Assert.IsNotNull(Person.FromCsvRow(goodRow));
+    }
+
+    [TestMethod]
+    public void FromCsvRow_AssignsCorrectValues()
+    {
+        string row = "1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577";
+        Person expectedRowPerson = new("Priscilla", "Jenyns", new Address("7884 Corry Way", "Helena", "MT", "70577"), "pjenyns0@state.gov");
+        Person rowPerson = Person.FromCsvRow(row);
+        (string firstName, string lastName, string email, string streetAddress, string city,
+            string state, string zipCode) = rowPerson;
+        Assert.AreEqual(firstName, expectedRowPerson.FirstName);
+        Assert.AreEqual(lastName, expectedRowPerson.LastName);
+        Assert.AreEqual(email, expectedRowPerson.EmailAddress);
+        Assert.AreEqual(streetAddress, expectedRowPerson.Address.StreetAddress);
+        Assert.AreEqual(city, expectedRowPerson.Address.City);
+        Assert.AreEqual(state, expectedRowPerson.Address.State);
+        Assert.AreEqual(zipCode, expectedRowPerson.Address.Zip);
+    }
+}
--- Assignment/Assignment/Person.cs
+++ Assignment/Assignment/Person.cs
@@ -1,19 +1,37 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace Assignment;
-public class Person : IPerson
+public class Person(string firstName, string lastName, IAddress address, string emailAddress) : IPerson
 {
-    public Person(string firstName, string lastName, IAddress address, string emailAddress)
+    public static Person FromCsvRow(string row)
     {
-        FirstName = firstName;
-        LastName = lastName;
-        Address = address;
-        EmailAddress = emailAddress;
+        ArgumentException.ThrowIfNullOrEmpty(row);
+        string[] columns = row.Split(',');
+        if(columns.Length != 8)
+        {
+            throw new Exception($"Row requires 8 columns, got {columns.Length} columns");
+        }
+        return new(columns[1], columns[2],
+            new Address(columns[4], columns[5], columns[6], columns[7]),
+            columns[3]);
     }
 
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public IAddress Address { get;set; }
-    public string EmailAddress { get; set; }
+    public void Deconstruct(out string FirstName, out string LastName, out string EmailAddress,
+        out string StreetAddress, out string City, out string State, out string Zip)
+    {
+        FirstName = this.FirstName;
+        LastName = this.LastName;
+        EmailAddress = this.EmailAddress;
+        StreetAddress = Address.StreetAddress;
+        City = Address.City;
+        State = Address.State;
+        Zip = Address.Zip;
+    }
+
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public IAddress Address { get; set; } = address;
+    public string EmailAddress { get; set; } = emailAddress;
 }
