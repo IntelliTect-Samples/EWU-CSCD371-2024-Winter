147c70bb22234d75c1da39b219ad2b6b885c38a7
rkdeboer <145617897+rkdeboer@users.noreply.github.com>
2024-02-26 12:47:12 -0800

    Fixed error in PErson

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -11,17 +11,17 @@ namespace Assignment
         private const string CsvFilePath = "People.csv";
 
         // 1.
-        public IEnumerable<string> CsvRows 
+        public IEnumerable<string> CsvRows
             => File.ReadLines(CsvFilePath).Skip(1);// This should return the entire list, Line by Line, as a string
-            
-         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
+
+        // 2.
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
             => CsvRows
                 .Select(row => row.Split(',')[6])
                 .Distinct()
                 .OrderBy(state => state);//This will use CsvRows, calling in one large string,
                                          //splitting it at the 6th comma, and ordering by state
-        
+
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
             => string.Join(",", GetUniqueSortedListOfStatesGivenCsvRows());//This should combine them,
@@ -35,27 +35,30 @@ namespace Assignment
                 return CsvRows.Select(row =>
                 {
                     var fields = row.Split(',');
-                    return new Person
-                    {
-                        FirstName = fields[1],
-                        LastName = fields[2],
-                        Address = new Address(
-                            streetAddress: fields[3],
-                            city: fields[4],
-                            state: fields[5],
-                            zip: fields[6]),
-                        EmailAddress = fields[7]
-                    };
-                }).OrderBy(p => p.Address.State)
-                .ThenBy(p => p.Address.City)
-                .ThenBy(p => p.Address.Zip)
-                .Cast<IPerson>();// This Implementation is a total guess, but it may be close.
+                    return new Person //There was an error with how the person was calling the constructor so I fixed that -R
+            (
+                firstName: fields[0],
+                lastName: fields[1],
+                address: new Address(
+                    streetAddress: fields[2],
+                    city: fields[3],
+                    state: fields[4],
+                    zip: fields[5]),
+                emailAddress: fields[6]
+            );
+
+                })
+        .Where(person => person != null) //This gets rid of null entries
+        .OrderBy(p => p.Address.State)
+        .ThenBy(p => p.Address.City)
+        .ThenBy(p => p.Address.Zip)
+        .Cast<IPerson>();
             }
         }
 
         // 5.
         public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
-            Predicate<string> filter) 
+            Predicate<string> filter)
             => People.Where(p => filter(p.EmailAddress))
             .Select(p => (p.FirstName, p.LastName)); //This one may be right, but again a first go.
 
