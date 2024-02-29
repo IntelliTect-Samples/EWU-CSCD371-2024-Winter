29cc00e32cab5db592ea3845a93c6e9c254700a4
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-02-28 21:18:06 -0800

    added failing test for checking CsvRows Property

--- Assignment/Assignment.Tests/ISampleDataTests.cs
+++ Assignment/Assignment.Tests/ISampleDataTests.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Assignment.Tests;
 
@@ -9,11 +13,13 @@ public class SampleDataTests
     //Properties are set in SetUp method so they will not be null
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public SampleData SampleData { get; set; }
+    public string PeopleCSVPath { get; private set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     [TestInitialize]
     public void SetUp()
     {
+        PeopleCSVPath = "people.csv";
         SampleData = new SampleData();
     }
 
@@ -21,7 +27,16 @@ public class SampleDataTests
     [TestMethod]
     public void CsvRows_PeopleCsv_ReturnsStringIEnumerable()
     {
+        // Arrange
+        IEnumerable<string> peopleCSV = SampleData.CsvRows;
+        using StreamReader writer = new(PeopleCSVPath);
 
+        // Act
+        writer.ReadLine();
+        string? firstRow = writer.ReadLine();
+
+        // Assert
+        Assert.Equals(peopleCSV.First(), firstRow);
     }
 }
 
