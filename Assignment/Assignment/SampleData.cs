94e82ab11540bceb36b0786fbaba12c9dc5e13c7
Purplepigs0129 <praythopson@ewu.edu>
2024-02-29 10:26:14 -0800

    1 somewhat complete will be finished when 7 is implemented

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows
+        {
+            get 
+            {
+                if(!File.Exists("People.csv"))
+                {
+                    throw new FileNotFoundException("People.csv not found");
+                }
+
+                var fileReader = new StreamReader("People.csv");
+                //Skips the 1st line in the file
+                fileReader.ReadLine();
+
+                while(!fileReader.EndOfStream)
+                {
+                    string? data = fileReader.ReadLine();
+                    if (data != null)
+                    {
+                        yield return data;
+                    }
+                }
+
+            }
+        }
 
         // 2.
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
