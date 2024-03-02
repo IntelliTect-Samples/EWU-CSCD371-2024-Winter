e7111ba38aca18c85e383ad98634178dddec6ffb
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-03-01 21:13:23 -0800

    added in Node class and test file from Assignment 5

---
+++ Assignment/Assignment.Tests/NodeTests.cs
@@ -0,0 +1,89 @@
+using Xunit;
+
+namespace GenericsHomework.Tests;
+
+public class NodeTests
+{
+
+    public Node<string> NodeList { get; }
+
+    public NodeTests() {
+        NodeList = new("Alexa");
+        NodeList.Append("is");
+        NodeList.Append("the");
+        NodeList.Append("best!");
+    }
+
+    [Fact]
+    public void Constructor_NullData_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new Node<string>(null!));
+    }
+
+    [Fact]
+    public void Append_NullData_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(()=>NodeList.Append(null!));
+    }
+
+    [Fact]
+    public void Exists_NullData_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => NodeList.Exists(null!));
+    }
+
+    [Fact]
+    public void ToString_ValidData_ReturnsStringSuccesfully()
+    {
+        Assert.Equal("Alexa", NodeList.ToString());
+        Assert.Equal("is", NodeList.Next.ToString());
+    }
+
+    [Fact]
+    public void Append_AddedNodes_SuccesfullyReturnsValue()
+    {
+        Node<string> newNode = new("Hiiii");
+        newNode.Append("You rock");
+        newNode.Append("!");
+
+        Assert.Equal("You rock", newNode.Next.Data);
+        Assert.Equal("!", newNode.Next.Next.Data);
+
+    }
+    [Fact]
+    public void Append_DuplicateNodeValue_ThrowsInvalidOperationException()
+    {
+        Node<string> newNode = new("Copy");
+        Assert.Throws<InvalidOperationException>(() => newNode.Append("Copy"));
+
+    }
+
+    [Fact]
+    public void Clear_ManyNodes_SuccessfullyClears()
+    {
+        Node<string> newNode = new("Lets goo");
+        newNode.Append("New Node1");
+        newNode.Append("New Node2");
+        newNode.Clear();
+
+        Assert.Same(newNode, newNode.Next);
+    }
+
+    [Theory]
+    [InlineData("best!")]
+    [InlineData("the")]
+    [InlineData("Alexa")]
+    public void Exists_NodeExists_ReturnsTrue(string data)
+    {
+        Assert.True(NodeList.Exists(data));
+    }
+
+
+    [Fact]
+    public void Exists_NodeDoesNotExists_ReturnsFalse()
+    {
+        Assert.False(NodeList.Exists("Rahat"));
+    }
+
+
+}
---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,88 @@
+﻿
+namespace GenericsHomework;
+
+    public class Node<T>
+    {
+        public Node(T item)
+        {
+            ArgumentNullException.ThrowIfNull(item, nameof(item)); 
+            Data = item;
+            Next = this;
+        }
+
+        public Node<T> Next { get; private set; }
+        // We made Data non nullable so precautions have been taken to ensure it can't get set
+        // to null
+        public T Data { get; }
+        
+        public override string ToString() 
+        {
+        // Data is ensured to be not null so null forgiviness is used.
+        return Data!.ToString()!;
+
+        }
+
+        public void Append(T item)
+        {
+            ArgumentNullException.ThrowIfNull(item,nameof(item));
+
+            if (Exists(item))
+            {
+                    throw new InvalidOperationException(nameof(item));
+             }
+            else
+            {
+                Node<T> currentNode = Next;
+
+                while (currentNode.Next != this)
+                {
+                    currentNode = currentNode.Next;
+                }
+   
+                Node<T> newNode = new(item)
+                {
+                    Next = this
+                };
+                    
+                currentNode.Next = newNode;
+            }
+        }
+
+    /* I think it is sufficient to set Next to itself because the garbage collector 
+    will be able to get back the memory and works with reference objects.
+    After some research as well, it seems the garbage collector works and handles
+    circular references and lists like our case here. 
+    The high level view of how the garbage collector works:
+    Start with locals, statics and GC pinned objects. None of these can be collected
+    Mark every object which can be reached by traversing the children of these objects Collect every object which is not marked.
+    This allows for circular references to be collected just fine. 
+    So long as none of them are reachable from an object known to be uncollectable then the circular reference is essentially irrelevant.
+    */
+    public void Clear()
+    {
+        Next = this;
+    }
+
+    public bool Exists(T item)
+    {
+        ArgumentNullException.ThrowIfNull(item, nameof(item));
+
+        Node<T> currentNode = this;
+
+    do
+    {
+        // Data is ensured to be not null so null forgiviness is used.
+        if (currentNode.Data!.Equals(item))
+        {
+            return true;
+        }
+
+        currentNode = currentNode.Next;
+    } while (currentNode != this);
+        return false;
+    }
+        
+    }
+
+
+
