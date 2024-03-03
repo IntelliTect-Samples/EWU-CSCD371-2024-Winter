ad7bd4c9187002e5dbbe7a0256c4d7b9fd430a2a
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-03-02 17:25:07 -0800

    added setup method  and SampleDataAsync property

--- Assignment/Assignment.Tests/SampleDataAsyncTests.cs
+++ Assignment/Assignment.Tests/SampleDataAsyncTests.cs
@@ -10,10 +10,20 @@ namespace Assignment.Tests;
 [TestClass]
 public class SampleDataAsyncTests
 {
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public SampleDataAsync SampleDataAsync { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        SampleDataAsync = new SampleDataAsync();
+    }
+
     [TestMethod]
     public void CsvRows_PeopleCsv_ReturnsListOfStrings()
     {
-        SampleDataAsync sampleDataAsync = new();
     }
 
 }
