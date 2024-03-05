02f660e2e0ff89228df5b2e4d10e77ee62ed1534
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-03-04 20:08:14 -0800

    updated project structure

---
+++ Assignment.Interfaces/IAddress.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment;
+
+public interface IAddress
+{
+    string StreetAddress { get; }
+    string City { get; }
+    string State { get; }
+    string Zip { get; }
+}
+
---
+++ Assignment.Interfaces/IPerson.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment;
+
+public interface IPerson
+{
+    string FirstName { get; }
+    string LastName { get; }
+    IAddress Address { get; }
+
+    string EmailAddress { get; }
+}
+
