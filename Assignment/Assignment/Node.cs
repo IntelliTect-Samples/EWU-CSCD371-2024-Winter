c2769049974d25377aed3147d92d986a65ac3b21
Trevor Stewart <tstewart12@ewu.edu>
2024-03-04 16:02:29 -0800

    Added Node and NodeTests from Assignment 5,
     Still need to implement IEnumerable

---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment;
+
+public class Node<T> : IEnumerable<Node<T>> where T : notnull
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
+    {
+        ArgumentNullException.ThrowIfNull(value, nameof(value));
+        Data = value;
+        Next = this;
+    }
+
+
+    public override string? ToString() => Data.ToString();
+
+    public void Append(T value)
+    {
+        //Prevent null values from being added
+        if (value == null) throw new ArgumentNullException(nameof(value));
+
+        //Throw an error on an attempt to Append a duplicate value.
+        if (Exists(value)) throw new ArgumentException("Cannot have duplicate: " + nameof(value));
+
+        Node<T> cur = this;
+
+        while (cur.Next != this)
+        {
+            cur = cur.Next;
+        }
+
+        Node<T> newNode = new(value);
+        cur.Next = newNode;
+        newNode.Next = this;
+    }
+
+    public void Clear() 
+    {
+        Node<T> cur = this;
+        cur.Next = this;
+    }
+
+    public bool Exists(T value)
+    {
+        //Search list to see if value exists
+        Node<T> cur = this;
+        do
+        {
+            if (cur.Data.Equals(value)) return true;
+            cur = cur.Next;
+
+        } while (cur != this);
+
+        return false;
+    }
+
+    public IEnumerator<Node<T>> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
