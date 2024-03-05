eb9d7abbcd908ec84659d7e5353ef3863f1ff5da
Purplepigs0129 <praythopson@ewu.edu>
2024-03-04 18:34:55 -0800

    Converted to MSTest and removed problematic test

---
+++ Assignment.Test/NodeTests.cs
@@ -0,0 +1,109 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Assignment.Test;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void Next_NodeLoopsOnItself_Equal()
+    {
+        Node<int> node = new(3);
+        Assert.AreEqual(node.Data, node.Next.Data);
+    }
+
+    [TestMethod]
+    public void Append_Node_Equal()
+    {
+        Node<int> node = new(6);
+        node.Append(7);
+        Assert.AreEqual(7, node.Next.Data);
+    }
+
+    [TestMethod]
+    public void Append_NodeWithDifferentValue_NotEqual()
+    {
+        Node<int> node = new(3);
+        node.Append(5);
+        Assert.AreNotEqual(node, node.Next);
+    }
+
+    [TestMethod]
+    public void Next_LoopsAroundToSelf_Equal()
+    {
+        Node<int> node = new(3);
+        node.Append(5);
+        Assert.AreEqual(node, node.Next.Next);
+    }
+
+    [TestMethod]
+    public void Exists_ValueInList_ReturnsTrue()
+    {
+        Node<string> node = new("apple");
+        node.Append("pear");
+        node.Append("orange");
+        Assert.IsTrue(node.Exists("pear"));
+    }
+
+    [TestMethod]
+    public void Exists_ValueInList_ReturnsFalse()
+    {
+        Node<string> node = new("apple");
+        node.Append("pear");
+        node.Append("orange");
+        Assert.IsFalse(node.Exists("cheese"));
+    }
+
+    [TestMethod]
+    public void Append_AppendsDuplicate_ThrowsException()
+    {
+        Node<int> node = new(3);
+        node.Append(5);
+        node.Append(7);
+        Assert.ThrowsException<ArgumentException>(() => node.Append(3));
+    }
+
+    [TestMethod]
+    public void Append_NullValue_ThrowsException()
+    {
+        Node<string> node = new("apple");
+        node.Append("pear");
+        node.Append("orange");
+        Assert.ThrowsException<ArgumentNullException>(() => node.Append(null!));
+    }
+
+    [TestMethod]
+    public void Clear_ClearsNewList_HeadRemains()
+    {
+        Node<string> node = new("apple");
+        node.Append("pear");
+        node.Append("orange");
+
+        //Verify the list has all items above
+        Assert.AreEqual("apple", node.Data);
+        Assert.AreEqual("pear", node.Next.Data);
+        Assert.AreEqual("orange", node.Next.Next.Data);
+
+        Assert.IsTrue(node.Exists("apple"));
+        Assert.IsTrue(node.Exists("pear"));
+        Assert.IsTrue(node.Exists("orange"));
+
+        //Clear the list and verify only start remains
+        node.Clear();
+        Assert.AreEqual("apple", node.Data);
+        Assert.AreNotEqual("pear", node.Next.Data);
+        Assert.AreNotEqual("orange", node.Next.Next.Data);
+
+        Assert.IsTrue(node.Exists("apple"));
+        Assert.IsFalse(node.Exists("pear"));
+        Assert.IsFalse(node.Exists("orange"));
+    }
+
+    [TestMethod]
+    public void ToString_Data_ReturnsSuccessful()
+    {
+        Node<string> node = new("Hello there");
+        Assert.AreEqual("Hello there", node.ToString());
+    }
+}
--- Assignment.Test/SampleDataTests.cs
+++ Assignment.Test/SampleDataTests.cs
@@ -1,88 +1,90 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Assignment.Tests;
+namespace Assignment.Test;
 
+[TestClass]
 public class SampleDataTests
 {
+
     public SampleData SampleData { get; set; }
-    public SampleDataTests() 
+
+    public SampleDataTests()
     {
         SampleData = new();
     }
 
-    [Fact]
-    public void CsvRows_SkipsFirstLine_ReturnsSuccessful()//
+    [TestMethod]
+    public void CsvRows_SkipsFirstLine_ReturnsSuccessful()
     {
         int CsvRows = SampleData.CsvRows.Count();
-        Assert.Equal(50, CsvRows);
+        Assert.AreEqual(50, CsvRows);
     }
-    /*
-    [Fact]
+
+    [TestMethod]
     public void GetUniqueSortedListOfStatesGivenCsvRows_SortedAndUnique_ReturnsSuccessful()
     {
         List<string> uniqueSortedStates = SampleData.GetUniqueSortedListOfStatesGivenCsvRows().ToList();
-        Assert.Equal("AL", uniqueSortedStates[0]);
-        Assert.Equal("WV", uniqueSortedStates[26]);
-    }*/
-    
-    [Fact]
+        Assert.AreEqual("AL", uniqueSortedStates[0]);
+        Assert.AreEqual("WV", uniqueSortedStates[26]);
+    }
+
+    [TestMethod]
     public void GetAggregateSortedListOfStatesUsingCsvRows_SortedAndUnique_ReturnsSuccessful()
     {
         string uniqueSortedStates = string.Join(", ", SampleData.GetAggregateSortedListOfStatesUsingCsvRows());
-        Assert.Equal("AL, AZ, CA, DC, FL, GA, IN, KS, LA, MD, MN, MO, MT, NC, NE, NH, NV, NY, OR, PA, SC, TN, TX, UT, VA, WA, WV", uniqueSortedStates);
+        Assert.AreEqual("AL, AZ, CA, DC, FL, GA, IN, KS, LA, MD, MN, MO, MT, NC, NE, NH, NV, NY, OR, PA, SC, TN, TX, UT, VA, WA, WV", uniqueSortedStates);
     }
 
-    [Fact]
-    public void People_CountIs50_ReturnsSuccessful()//
+    [TestMethod]
+    public void People_CountIs50_ReturnsSuccessful()
     {
         var people = SampleData.People.ToList();
 
-        Assert.Equal(50, people.Count);
+        Assert.AreEqual(50, people.Count);
     }
-    /*
+
     // Filtered by .cn
-    [Fact]
+    [TestMethod]
     public void FilterByEmailAddress_ContainsCn_ReturnsSuccessful()
     {
         List<(string FirstName, string LastName)> govEmails = SampleData.FilterByEmailAddress(email => email.Contains(".cn")).ToList();
-        Assert.Equal(2, govEmails.Count);
+        Assert.AreEqual(2, govEmails.Count);
     }
-    */
+
     // Filtered by .com
-    [Fact]
-    public void FilterByEmailAddress_ContainsCom_ReturnsSuccessful()//
+    [TestMethod]
+    public void FilterByEmailAddress_ContainsCom_ReturnsSuccessful()
     {
         List<(string FirstName, string LastName)> comEmails = SampleData.FilterByEmailAddress(email => email.Contains(".com")).ToList();
-        Assert.Equal(29, comEmails.Count);
+        Assert.AreEqual(29, comEmails.Count);
     }
 
     // Filtered by .gov
-    [Fact]
-    public void FilterByEmailAddress_ContainsGov_ReturnsSuccessful()//
+    [TestMethod]
+    public void FilterByEmailAddress_ContainsGov_ReturnsSuccessful()
     {
         List<(string FirstName, string LastName)> govEmails = SampleData.FilterByEmailAddress(email => email.Contains(".gov")).ToList();
-        Assert.Equal(5, govEmails.Count);
+        Assert.AreEqual(5, govEmails.Count);
     }
-    /*
+
     // GetAggregateListOfStatesGivenPeopleCollection for Unique States
-    [Fact]
+    [TestMethod]
     public void GetAggregateListOfStatesGivenPeopleCollection_UniqueList_ReturnsSuccessful()
     {
         List<IPerson> people = SampleData.People.ToList();
         string states = SampleData.GetAggregateListOfStatesGivenPeopleCollection(people);
-        Assert.Equal("AL, AZ, CA, DC, FL, GA, IN, KS, LA, MD, MN, MO, MT, NC, NE, NH, NV, NY, OR, PA, SC, TN, TX, UT, VA, WA, WV", states);
+        Assert.AreEqual("AL, AZ, CA, DC, FL, GA, IN, KS, LA, MD, MN, MO, MT, NC, NE, NH, NV, NY, OR, PA, SC, TN, TX, UT, VA, WA, WV", states);
     }
 
     // GetAggregateListOfStatesGivenPeopleCollection with empty list
-    [Fact]
+    [TestMethod]
     public void GetAggregateListOfStatesGivenPeopleCollection_EmptyList_ReturnsSuccessful()
     {
         List<IPerson> people = [];
         string states = SampleData.GetAggregateListOfStatesGivenPeopleCollection(people);
-        Assert.Equal("", states);
+        Assert.AreEqual("", states);
     }
-    */
 
 }
