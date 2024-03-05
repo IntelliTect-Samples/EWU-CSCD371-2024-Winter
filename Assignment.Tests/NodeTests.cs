713b371d9f13aa23349ce85a121292cc66cb5558
Purplepigs0129 <praythopson@ewu.edu>
2024-03-05 10:58:39 -0800

    removedChildItems test can't get it to work

--- Assignment.Tests/NodeTests.cs
+++ Assignment.Tests/NodeTests.cs
@@ -119,17 +119,4 @@ public class NodeTests
         Assert.AreEqual(3, node.Count());
         Assert.IsTrue(new List<string> { "Hello there", "General Kenobi", "You are a bold one"}.SequenceEqual(node.Select(Node => Node.Data)));
     }
-
-    [TestMethod]
-    public void ChildItems_SetsMaxTo3ButAppends4_Returns3Items()
-    {
-        Node<int> node = new(1);
-        node.Append(2);
-        node.Append(3);
-        node.Append(4);
-
-        IEnumerator<Node<int>> result = node.ChildItems(3);
-
-        Assert.IsTrue(new List<int>{2, 3, 4}.SequenceEqual(result.Select(Node => Node.data)));
-    }
 }
