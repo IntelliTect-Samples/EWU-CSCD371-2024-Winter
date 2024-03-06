ba5015bce35ea1b33f8b1df275fbd42ae33005be
Hadi <amallah@ewu.edu>
2024-03-05 22:28:17 -0800

    MEOW

---
+++ Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Assignment.Tests;
+public class SampleDataTests
+{
+    [Fact]
+    public void SampleTest()
+    {
+        string str = "meow";
+        Assert.Equal("meow", str);
+    }
+}
+
+
+
