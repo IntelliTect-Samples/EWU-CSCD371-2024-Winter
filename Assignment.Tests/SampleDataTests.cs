2923d65ad0c0e480046406b2540b7698df44c2a6
rahat15 <rbhatia@ewu.edu>
2024-03-07 23:50:52 -0800

    better tests

--- Assignment.Tests/SampleDataTests.cs
+++ Assignment.Tests/SampleDataTests.cs
@@ -1,17 +1,4 @@
-﻿//using System;
-//using Xunit;
-
-//namespace Assignment.Tests;
-//public class SampleDataTests
-//{
-//    [Fact]
-//    public void SampleTest()
-//    {
-//        string str = "meow";
-//        Assert.Equal("meow", str);
-//    }
-//}
-
+﻿
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -21,11 +8,12 @@ using Xunit;
 
 namespace Assignment.Tests;
 
+//had to remove underscores as it gave build errors in github
 public class SampleDataTests
 {
     // Test for verifying the correctness of CsvRows property output
     [Fact]
-    public void CsvRows_ReturnsCorrectOutput()
+    public void CsvRowsReturnsCorrectOutput()
     {
         IEnumerable<string> csvRows = new SampleData().CsvRows.ToList();
         Assert.Equal(50, csvRows.Count());
@@ -33,7 +21,7 @@ public class SampleDataTests
 
     // Test for verifying the correctness of output from GetUniqueSortedListOfStatesGivenCsvRows method
     [Fact]
-    public void GetUniqueSortedListOfStatesGivenCsvRows_ReturnsCorrectOutput()
+    public void GetUniqueSortedListOfStatesGivenCsvRowsReturnsCorrectOutput()
     {
         IEnumerable<string> states = new SampleData().GetUniqueSortedListOfStatesGivenCsvRows().ToList();
         bool isSorted = states.Zip(states, (first, second) => String.Compare(first, second, StringComparison.Ordinal) < 0 || first.Equals(second, StringComparison.Ordinal)).All(rows => rows);
@@ -45,7 +33,7 @@ public class SampleDataTests
     [InlineData("FL")]
     [InlineData("CA")]
     [Theory]
-    public void GetUniqueSortedListOfStatesGivenCsvRows_HardCodedValues_ReturnsCorrect(string row)
+    public void GetUniqueSortedListOfStatesGivenCsvRowsHardCodedValuesReturnsCorrect(string row)
     {
         IEnumerable<string> states = new SampleData().GetUniqueSortedListOfStatesGivenCsvRows().ToList();
         Assert.Contains(row, states);
@@ -53,7 +41,7 @@ public class SampleDataTests
 
     // Test for verifying the correctness of output from GetAggregateSortedListOfStatesUsingCsvRows method
     [Fact]
-    public void GetAggregateSortedListOfStatesUsingCsvRows_ReturnsCorrectOutput()
+    public void GetAggregateSortedListOfStatesUsingCsvRowsReturnsCorrectOutput()
     {
         string states = new SampleData().GetAggregateSortedListOfStatesUsingCsvRows();
         Assert.Equal("AL, AZ, CA, DC, FL, GA, IN, KS, LA, MD, MN, MO, MT, NC, NE, NH, NV, NY, OR, PA, SC, TN, TX, UT, VA, WA, WV", states);
@@ -61,7 +49,7 @@ public class SampleDataTests
 
     // Test for verifying the number of people returned by the People property
     [Fact]
-    public void People_ReturnsCorrectNumberOfPeople()
+    public void PeopleReturnsCorrectNumberOfPeople()
     {
         IEnumerable<IPerson> peopleList = new SampleData().People;
         Assert.Equal(50, peopleList.Count());
@@ -69,7 +57,7 @@ public class SampleDataTests
 
     // Test for verifying the correctness of the first person returned by the People property
     [Fact]
-    public void People_FirstPerson_IsCorrect()
+    public void People_FirstPersonIsCorrect()
     {
         IEnumerable<IPerson> peopleList = new SampleData().People;
         Assert.Equal("Arthur", peopleList.ElementAt(0).FirstName);
@@ -77,7 +65,7 @@ public class SampleDataTests
 
     // Test for verifying filtering of people by email address
     [Fact]
-    public void People_FilterByEmail_ReturnsCorrectFirstPerson()
+    public void People_FilterByEmailReturnsCorrectFirstPerson()
     {
         IEnumerable<(string firstname, string lastname)> filteredPeople = new SampleData()
             .FilterByEmailAddress(email => email.Contains(".edu"));
@@ -87,7 +75,7 @@ public class SampleDataTests
 
     // Test for verifying the correctness of states returned by GetAggregateListOfStatesGivenPeopleCollection method
     [Fact]
-    public void GetAggregateListOfStatesGivenPeopleCollection_ReturnsCorrectListOfStates()
+    public void GetAggregateListOfStatesGivenPeopleCollectionReturnsCorrectListOfStates()
     {
         IEnumerable<IPerson> people = new SampleData().People;
         IEnumerable<string> aggregatedStates = new SampleData()
