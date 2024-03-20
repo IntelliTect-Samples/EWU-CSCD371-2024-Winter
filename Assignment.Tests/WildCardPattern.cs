1ed6e69d8b0bd65d1c1884393e5f2f7a0f701ea6
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-03-19 22:39:42 -0700

    fixing errors

--- Assignment.Tests/WildCardPattern.cs
+++ Assignment.Tests/WildCardPattern.cs
@@ -1013,7 +1013,7 @@ internal sealed class WildcardPatternMatcher
         }
     }
 
-    private class LiteralCharacterElement : QuestionMarkElement
+    private sealed class LiteralCharacterElement : QuestionMarkElement
     {
         private readonly char _literalCharacter;
 
@@ -1039,7 +1039,7 @@ internal sealed class WildcardPatternMatcher
         }
     }
 
-    private class BracketExpressionElement : QuestionMarkElement
+    private sealed class BracketExpressionElement : QuestionMarkElement
     {
         private readonly Regex _Regex;
 
@@ -1063,7 +1063,7 @@ internal sealed class WildcardPatternMatcher
         }
     }
 
-    private class AsterixElement : PatternElement
+    private sealed class AsterixElement : PatternElement
     {
         public override void ProcessStringCharacter(
                         char currentStringCharacter,
@@ -1087,7 +1087,7 @@ internal sealed class WildcardPatternMatcher
         }
     }
 
-    private class MyWildcardPatternParser : WildcardPatternParser
+    private sealed class MyWildcardPatternParser : WildcardPatternParser
     {
         private readonly List<PatternElement> _patternElements = new();
         private CharacterNormalizer _characterNormalizer;
@@ -1190,7 +1190,7 @@ internal sealed class WildcardPatternMatcher
 /// <summary>
 /// Translates a <see cref="WildcardPattern"/> into a DOS wildcard
 /// </summary>
-internal class WildcardPatternToDosWildcardParser : WildcardPatternParser
+internal sealed class WildcardPatternToDosWildcardParser : WildcardPatternParser
 {
     private readonly StringBuilder _result = new();
 
